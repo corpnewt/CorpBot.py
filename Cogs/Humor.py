3783f95b2e65d0812eb2be1fcc89fd082bb93b33
Zoey Cluff <matada89@gmail.com>
2016-10-31 20:11:07 -0500

    add a present

--- Cogs/Humor.py
+++ Cogs/Humor.py
@@ -9,12 +9,12 @@ class Humor:
 
 	def __init__(self, bot):
 		self.bot = bot
-		
+
 	def message(self, message):
 		# Check the message and see if we should allow it - always yes.
 		# This module doesn't need to cancel messages.
 		return { 'Ignore' : False, 'Delete' : False}
-		
+
 	@commands.command(pass_context=True)
 	async def fart(self, ctx):
 		"""PrincessZoey :P"""
@@ -22,3 +22,11 @@ class Humor:
 		randnum = random.randint(0, len(fartList)-1)
 		msg = '{}'.format(fartList[randnum])
 		await self.bot.send_message(ctx.message.channel, msg)
+
+	@commands.command(pass_context=True)
+	async def shart(self, ctx):
+	"""PrincessZoey was here <3"""
+	shartList = ["Oops not again!", "Son of a biscuit eating whore", "Really? God Damnit",]
+	randnum = random.randinit(0, len(fartList)-1)
+	msg = '{}'.format(fartList[randnum])
+	await self.bot.send_message(ctx.message.channel, msg)
