b47d190783281a35c3fb7ec1cefa5ef5a5b9601c
CorpNewt <corporatenewt@gmail.com>
2016-11-06 16:00:36 -0600

    More Dr. Beer

--- Cogs/DrBeer.py
+++ Cogs/DrBeer.py
@@ -1,6 +1,6 @@
 import asyncio
 import discord
-from   operator import itemgetter
+import random
 from   discord.ext import commands
 
 # This is the Uptime module. It keeps track of how long the bot's been up
@@ -19,5 +19,12 @@ class DrBeer:
 	@commands.command(pass_context=True)
 	async def drbeer(self, ctx):
 		"""Put yourself in your place."""
-		msg = "Hey, yall. Quit ya horsin' around now. Can't you see I'm busy tryin'a shoot'n all them summersquash?"
+		beerList = ["Hey, yall. Quit ya horsin' around now. Can't you see I'm busy tryin'a shoot'n all them summersquash?",
+					"Now I don't know how to use all them 5-dollah words y'all sprayin' around, but sure seems to me like y'all need to mind your peas and queues.",
+					"As long as I can keep practicin' and protectin' all my favorite amendments, like the second and thirty-first, I am all dandy.",
+					"Woah there, buckaroo! That's a mighty harsh language from someone communicating through a tube in the ocean over the internets.",
+					"Now, I don't mind y'all people, but you keep botherin' me when I'm tryin'a enjoy my cold Bud in this beautiful, patriotic sunset. Haven't yall folks got better things to do then keep arguing and snicker in' around when y'all should be worried about the government and the N, S & A listenin'?",
+					"Well, my daddy always said a man is only as good as his words and the thrust and torque of his good ole John Deere."]
+		randnum = random.randint(0, len(beerList)-1)
+		msg = '{}'.format(beerList[randnum])
 		await self.bot.send_message(ctx.message.channel, msg)
