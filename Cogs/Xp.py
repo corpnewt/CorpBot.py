672676acceb67b3de1259725846e9cd94497d387
CorpNewt <corporatenewt@gmail.com>
2016-11-22 14:47:04 -0600

    Update Xp.py

--- Cogs/Xp.py
+++ Cogs/Xp.py
@@ -509,8 +509,8 @@ class Xp:
 			nextRole = None
 
 		for role in promoSorted:
-			print("{} : {}".format(int(nextRole['XP']), newStat)
-			if nextRole['XP'] < newStat:
+			print("{} : {}".format(int(nextRole['XP']), newStat))
+			if int(nextRole['XP']) < newStat:
 				nextRole = role
 				print("New: {}".format(role))
 			# We *can* have this role, let's see if we already do
