a6e76fd5ebe63dbafd1ccf6c963318c6bc937668
CorpNewt <corporatenewt@gmail.com>
2016-12-08 00:03:03 -0600

    $shrug $lenny orig message delete

--- Cogs/Face.py
+++ Cogs/Face.py
@@ -13,11 +13,15 @@ class Face:
 	@commands.command(pass_context=True)
 	async def lenny(self, ctx):
 		"""Give me some Lenny."""
+		# Remove original message
+		await self.bot.delete_message(ctx.message)
 		await self.bot.send_message(ctx.message.channel, "( ͡° ͜ʖ ͡°)")
 
 	@commands.command(pass_context=True)
 	async def shrug(self, ctx):
 		"""Shrug it off."""
+		# Remove original message
+		await self.bot.delete_message(ctx.message)
 		await self.bot.send_message(ctx.message.channel, "¯\_(ツ)_/¯")
 
 	
