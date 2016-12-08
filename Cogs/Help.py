6b1141e6be9b673aa8c021c278d0ef6229d94551
CorpNewt <corporatenewt@gmail.com>
2016-12-08 13:46:34 -0600

    Remove message

--- Cogs/Help.py
+++ Cogs/Help.py
@@ -12,9 +12,4 @@ class Help:
 		# cogs = an array of the cogs used to start the bot
 		self.bot = bot
 		self.cogs = cogs
-		
-	def message(self, message):
-		# Check the message and see if we should allow it - always yes.
-		# This module doesn't need to cancel messages.
-		return { 'Ignore' : False, 'Delete' : False}
 	
