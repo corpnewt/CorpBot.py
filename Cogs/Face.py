515f60a4eda2c2a162bf710a2ba5c5728b257fbb
CorpNewt <corporatenewt@gmail.com>
2016-12-09 15:07:22 -0600

    Optional messages

--- Cogs/Face.py
+++ Cogs/Face.py
@@ -11,17 +11,23 @@ class Face:
 		self.bot = bot
 
 	@commands.command(pass_context=True)
-	async def lenny(self, ctx):
+	async def lenny(self, ctx, *, message : str = None):
 		"""Give me some Lenny."""
 		# Remove original message
 		await self.bot.delete_message(ctx.message)
-		await self.bot.send_message(ctx.message.channel, "( ͡° ͜ʖ ͡°)")
+		msg = "( ͡° ͜ʖ ͡°)"
+		if message:
+			msg += "\n{}".format(message)
+		await self.bot.send_message(ctx.message.channel, msg)
 
 	@commands.command(pass_context=True)
-	async def shrug(self, ctx):
+	async def shrug(self, ctx, *, message : str = None):
 		"""Shrug it off."""
 		# Remove original message
 		await self.bot.delete_message(ctx.message)
-		await self.bot.send_message(ctx.message.channel, "¯\_(ツ)_/¯")
+		msg = "¯\_(ツ)_/¯"
+		if message:
+			msg += "\n{}".format(message)
+		await self.bot.send_message(ctx.message.channel, msg)
 
 	
