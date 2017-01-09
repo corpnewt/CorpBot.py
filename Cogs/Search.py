6c63dc8150c523dcab9a736f2b56bb5ef8c1d749
Mercurial <clark_alesna@hotmail.com>
2017-01-09 18:26:22 +0000

    Added  command to search content from corpnewt.com forums

--- Cogs/Search.py
+++ Cogs/Search.py
@@ -5,6 +5,9 @@ from   discord.ext import commands
 from   Cogs import Settings
 from   Cogs import DisplayName
 from   Cogs import TinyURL
+import urllib
+import requests
+import json
 
 class Search:
 
@@ -56,3 +59,57 @@ class Search:
 		msg = '*{}*, you can find your answers here:\n\n{}'.format(DisplayName.name(ctx.message.author), lmgtfyT)
 		# Say message
 		await self.bot.send_message(ctx.message.channel, msg)
+
+
+	@commands.command(pass_context=True)
+	async def searchsite(self, ctx, category_name = None, query = None):
+		"""Search corpnewt.com forums."""
+
+                #Hey Corp replace this with the authorization code :D
+		auth = '<replace authorizaton code here>'
+
+		if query == None or category_name == None:
+			msg = "Usage: $searchsite [category] [search term]\n\n Categories can be found at:\n\nhttps://corpnewt.com/"
+			await self.bot.send_message(ctx.message.channel, msg)
+			return
+
+		categories_url = "https://corpnewt.com/api/categories"
+		r = requests.get(categories_url, headers={'Authorization': auth})
+		categories_json = json.loads(r.text)
+		categories = categories_json["categories"]
+
+		category = await self.find_category(categories, category_name)
+
+		if category == None:
+			await self.bot.say("Usage: $searchsite [category] [search term]\n\n Categories can be found at:\n\nhttps://corpnewt.com/")
+			return
+
+		search_url = "https://corpnewt.com/api/search?term={}&in=titlesposts&categories[]={}&searchChildren=true&showAs=posts".format(query, category["cid"])
+		r = requests.get(search_url, headers={'Authorization': auth})
+		search_json = json.loads(r.text)
+		posts = search_json["posts"]
+		result_string = '**Found {} Results for: {}**\n\n'.format(len(posts), query)
+
+		for post in posts:
+			result_string += '```{}``` \n\n https://corpnewt.com/{}\n\n'.format(post["topic"]["title"], post["topic"]["slug"])
+			
+		await self.bot.say(result_string)
+
+
+	async def find_category(self, categories, category_to_search):
+		"""recurse through the categories and sub categories to find the correct category"""
+		result_category = None
+		
+		for category in categories:
+                        
+                        print(category["name"].lower())
+                        if str(category["name"].lower()).strip() == str(category_to_search.lower()).strip():
+                                return category
+
+                        if len(category["children"]) > 0:
+                                result_category = await self.find_category(category["children"], category_to_search)
+                                if result_category != None:
+                                        return result_category
+		
+		return result_category
+
