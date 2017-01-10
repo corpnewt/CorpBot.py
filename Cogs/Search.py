01473a83e366084027bd4ea6379a02cb52aac1a0
CorpNewt <corporatenewt@gmail.com>
2017-01-10 00:50:27 -0600

    Typo fix

--- Cogs/Search.py
+++ Cogs/Search.py
@@ -105,7 +105,7 @@ class Search:
 		for post in posts:
 			if ctr < limit:
 				ctr = ctr + 1
-				result_string += '__{}__\nhttps://corpnewt.com/{}\n\n'.format(post["topic"]["title"], post["topic"]["slug"])
+				result_string += '__{}__\nhttps://corpnewt.com/topic/{}\n\n'.format(post["topic"]["title"], post["topic"]["slug"])
 			
 		await self.bot.say(result_string)
 
