7ad6015ecc7457eeffb7cd87a520d3f3b58b8445
Mercurial <clark_alesna@hotmail.com>
2017-01-30 07:46:03 +0000

    Impelemented meme and memetemps command

--- Cogs/Humor.py
+++ Cogs/Humor.py
@@ -1,7 +1,11 @@
 import asyncio
 import discord
 import random
+import urllib
+import requests
+import json
 from   discord.ext import commands
+from Cogs import Message
 
 # This module is for random funny things I guess...
 
@@ -17,3 +21,33 @@ class Humor:
 		randnum = random.randint(0, len(fartList)-1)
 		msg = '{}'.format(fartList[randnum])
 		await self.bot.send_message(ctx.message.channel, msg)
+
+	@commands.command(pass_context=True)
+	async def memetemps(self, ctx):
+		"""Get Meme Templates"""
+		url = "https://api.imgflip.com/get_memes"
+		r = requests.get(url)
+		result_json = json.loads(r.text)
+		templates = result_json["data"]["memes"]
+
+		templates_string = "**Meme Templates**\n"
+		for template in templates:
+			templates_string += "* [`{}` - `{}`]\n".format(template["id"], template["name"])
+		
+		await Message.say(self.bot, templates_string, ctx.message.author)
+
+	@commands.command(pass_context=True)
+	async def meme(self, ctx, template_id = None, text_zero = None, text_one = None):
+		"""Generate Meme"""
+
+		if template_id == None or text_zero == None or text_one == None:
+			msg = "Usage: `{}meme [template_id] [text#1] [text#2]`\n\n Meme Templates can be found using `$memetemps`".format(ctx.prefix)
+			await self.bot.send_message(ctx.message.channel, msg)
+			return
+
+		url = "https://api.imgflip.com/caption_image"
+		payload = {'template_id': template_id, 'username':'CorpBot', 'password': 'pooter123', 'text0': text_zero, 'text1': text_one }
+		r = requests.post(url, data=payload)
+		result_json = json.loads(r.text)
+		result = result_json["data"]["url"]
+		await self.bot.send_message(ctx.message.channel, result)
