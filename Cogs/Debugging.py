88b1284cd67c2eebfb8dd48ec821979de0e4e81c
CorpNewt <corporatenewt@gmail.com>
2017-04-07 12:38:53 -0500

    Setup logging (I hope)

--- Cogs/Debugging.py
+++ Cogs/Debugging.py
@@ -4,6 +4,7 @@ import os
 from   datetime import datetime
 from   operator import itemgetter
 from   discord.ext import commands
+from   Cogs import DisplayName
 
 # This is the Debugging module. It keeps track of how long the bot's been up
 
@@ -46,6 +47,173 @@ class Debugging:
 				msg = msg.encode("utf-8")
 				with open("debug.txt", "wb") as myfile:
 					myfile.write(msg)
+					
+	async def onjoin(self, member, server):
+		# A new member joined
+		msg = '*{}#{}* joined *{}*.'.format(member.name, member.discriminator, server.name)
+		logLevel = 0
+		await self._logEvent(server, msg, logLevel)
+		
+	async def onleave(self, member, server):
+		# A member left
+		msg = '*{}#{}* left *{}*.'.format(member.name, member.discriminator, server.name)
+		logLevel = 0
+		await self._logEvent(server, msg, logLevel)
+		
+	async def status(self, before, after):
+		# A member changed something about their user-profile
+		if not str(before.status.lower()) == str(after.status.lower()):
+			msg = '*{}#{}* went from *{}* to *{}*.'.format(before.name, before.discriminator, before.status.lower(), after.status.lower())
+			logLevel = 1
+			await self._logEvent(server, msg, logLevel)
+		if not before.game == after.game:
+			# Something changed
+			msg = '*{}#{}* changed playing status: ```\n'.format(before.name, before.discriminator)
+			if not before.game.name == after.game.name:
+				# Name change
+				msg += 'Name:\n   {}\n   --->\n   {}'.format(before.game.name, after.game.name)
+			if not before.game.url == after.game.url:
+				# URL changed
+				msg += 'URL:\n   {}\n   --->\n   {}'.format(before.game.url, after.game.url)
+			if not before.game.type == after.game.type:
+				# Type changed
+				msg += 'Type:\n   {}\n   --->\n   {}'.format(before.game.type, after.game.type)
+			msg += '```'
+			logLevel = 1
+			await self._logEvent(server, msg, logLevel)
+		if not before.avatar_url == after.avatar_url:
+			# Avatar changed
+			msg = '*{}#{}* changed avatars: ```\n{}\n   --->\n{}'.format(before.name, before.discriminator, before.avatar_url, after.avatar_url)
+			logLevel = 1
+			await self._logEvent(server, msg, logLevel)
+		if not before.nick == after.nick:
+			# Nickname changed
+			msg = '*{}#{}* changed nickname: ```\n{}\n   --->\n{}'.format(before.name, before.discriminator, before.nick, after.nick)
+			logLevel = 1
+			await self._logEvent(server, msg, logLevel)
+		if not before.name == after.name:
+			# Name changed
+			msg = '*{}#{}* changed name: ```\n{}\n   --->\n{}'.format(before.name, before.discriminator, before.name, after.name)
+			logLevel = 1
+			await self._logEvent(server, msg, logLevel)
+		
+	async def message(self, message):
+		# A message was sent
+		msg = '*{}#{}* sent: ```\n{}```'.format(message.author.name, message.author.discriminator, message.content)
+		logLevel = 2
+		await self._logEvent(message.server, msg, logLevel)
+		
+	async def message_edit(self, before, after):
+		# A message was edited
+		msg = '*{}#{}* edited: ```\n{}```'.format(before.author.name, before.author.discriminator, before.content)
+		logLevel = 1
+		await self._logEvent(before.server, msg, logLevel)
+		msg = 'To: ```\n{}```'.format(after.content)
+		await self._logEvent(before.server, msg, logLevel)
+		
+	async def message_delete(self, message):
+		# A message was deleted
+		msg = '*{}#{}* deleted: ```\n{}```'.format(message.author.name, message.author.discriminator, message.content)
+		logLevel = 1
+		await self._logEvent(message.server, msg, logLevel)
+	
+	async def _logEvent(self, server, log_message, log_level = 0):
+		# Here's where we log our info
+		# Get log channel
+		logChanID = self.settings.getServerStat(server, "LogChannel")
+		if not logChanID:
+			return
+		logChan = self.bot.get_channel(logChanID)
+		if not logChan:
+			return
+		# At this point - we have a log channel - get/compare the log levels
+		logLevel  = self.settings.getServerStat(server, "LogLevel")
+		if log_level > logLevel:
+			# Too verbose
+			return
+		# At this point - we log the message
+		await self.bot.send_message(logChan, log_message)
+	
+	
+	@commands.command(pass_context=True)
+	async def loglevel(self, ctx, *, log = None):
+		"""Sets the server's logging level (0 = quiet, 1 = normal, 2 = verbose [bot-admin only])."""
+
+		author  = ctx.message.author
+		server  = ctx.message.server
+		channel = ctx.message.channel
+		
+		isAdmin = ctx.message.author.permissions_in(ctx.message.channel).administrator
+		if not isAdmin:
+			checkAdmin = self.settings.getServerStat(ctx.message.server, "AdminArray")
+			for role in ctx.message.author.roles:
+				for aRole in checkAdmin:
+					# Get the role that corresponds to the id
+					if aRole['ID'] == role.id:
+						isAdmin = True
+		# Only allow admins to change server stats
+		if not isAdmin:
+			await self.bot.send_message(ctx.message.channel, 'You do not have sufficient privileges to access this command.')
+			return
+
+		logLevel  = self.settings.getServerStat(server, "LogLevel")
+
+		if log == None:
+			# Output log level
+			if logLevel == 2:
+				await self.bot.send_message(ctx.message.channel, 'Log level is *verbose*.')
+			elif logLevel == 1:
+				await self.bot.send_message(ctx.message.channel, 'Log level is *normal*.')
+			else:
+				await self.bot.send_message(ctx.message.channel, 'Log level is *quiet*.')
+			return
+		elif log.lower() == "0" or log.lower() == "quiet":
+			self.settings.setServerStat(server, "LogLevel", 0)
+			await self.bot.send_message(ctx.message.channel, 'Log level set to *quiet*.')
+		elif log.lower() == "1" or log.lower() == "normal":
+			self.settings.setServerStat(server, "LogLevel", 1)
+			await self.bot.send_message(ctx.message.channel, 'Log level set to *normal*.')
+		elif log.lower() == "2" or log.lower() == "verbose":
+			self.settings.setServerStat(server, "LogLevel", 2)
+			await self.bot.send_message(ctx.message.channel, 'Log level set to *verbose*.')
+			
+			
+	@commands.command(pass_context=True)
+	async def setlogchannel(self, ctx, *, channel : discord.Channel = None):
+		"""Sets the channel for Logging (bot-admin only)."""
+		
+		isAdmin = ctx.message.author.permissions_in(ctx.message.channel).administrator
+		if not isAdmin:
+			checkAdmin = self.settings.getServerStat(ctx.message.server, "AdminArray")
+			for role in ctx.message.author.roles:
+				for aRole in checkAdmin:
+					# Get the role that corresponds to the id
+					if aRole['ID'] == role.id:
+						isAdmin = True
+		# Only allow admins to change server stats
+		if not isAdmin:
+			await self.bot.send_message(ctx.message.channel, 'You do not have sufficient privileges to access this command.')
+			return
+
+		if channel == None:
+			self.settings.setServerStat(ctx.message.server, "LogChannel", "")
+			msg = 'Logging is now *disabled*.'
+			await self.bot.send_message(ctx.message.channel, msg)
+			return
+
+		# If we made it this far - then we can add it
+		self.settings.setServerStat(ctx.message.server, "LogChannel", channel.id)
+
+		msg = 'Logging is now *enabled* in **{}**.'.format(channel.name)
+		await self.bot.send_message(ctx.message.channel, msg)
+		
+	
+	@setlogchannel.error
+	async def setlogchannel_error(self, ctx, error):
+		# do stuff
+		msg = 'setlogchannel Error: {}'.format(ctx)
+		await self.bot.say(msg)
+	
 
 	@commands.command(pass_context=True)
 	async def setdebug(self, ctx, *, debug = None):
