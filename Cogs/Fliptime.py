f1c4dfaf01c68be1ab1a6ec33020df2b3165563a
CorpNewt <corporatenewt@gmail.com>
2017-04-07 11:33:49 -0500

    Update Fliptime.py

--- Cogs/Fliptime.py
+++ Cogs/Fliptime.py
@@ -15,7 +15,7 @@ class Fliptime:
 		self.bot = bot
 		self.settings = settings
 
-	async def message_edit(self, message):
+	async def message_edit(self, before_message, message):
 		# Pipe the edit into our message func to respond if needed
 		await self.message(message)
 		
