c3820c67595bcd4bd6fdb4694d2d958b7c861b16
CorpNewt <corporatenewt@gmail.com>
2017-04-07 15:40:14 -0500

    Update Debugging.py

--- Cogs/Debugging.py
+++ Cogs/Debugging.py
@@ -15,6 +15,15 @@ class Debugging:
 		self.bot = bot
 		self.settings = settings
 		self.debug = debug
+		self.logvars = [ 'user', 'user.join', 'user.leave', 'user.status', 'user.game',
+				'user.game.name', 'user.game.url', 'user.game.type', 'user.avatar',
+				'user.nick', 'user.name', 'message', 'message.send', 'message.delete',
+				'message.edit' ]
+		self.quiet = [ 'user.join', 'user.leave' ]
+		self.normal = [ 'user.join', 'user.leave', 'user.avatar', 'user.nick', 'user.name',
+			       'message.edit', 'message.delete' ]
+		self.verbose = [ 'user', 'message' ]
+		
 
 	async def oncommand(self, command, ctx):
 		if self.debug:
@@ -48,53 +57,77 @@ class Debugging:
 				with open("debug.txt", "wb") as myfile:
 					myfile.write(msg)
 					
+	def shouldLog(self, logVar, server):
+		serverLogVars = self.settings.getServerStat(server, "LogVars")
+		checks = logVar.split('.')
+		check = ''
+		for item in checks
+			if len(check):
+				check += '.' += item
+			else:
+				check = item
+			if check.lower() in serverLogVars:
+				return True
+		return False
+			
+			
 	async def onjoin(self, member, server):
+		if not self.shouldLog('user.join', server):
+			return
 		# A new member joined
 		msg = '*{}#{}* joined *{}*.'.format(member.name, member.discriminator, server.name)
 		logLevel = 0
 		await self._logEvent(server, msg, logLevel)
 		
 	async def onleave(self, member, server):
+		if not self.shouldLog('user.leave', server):
+			return
 		# A member left
 		msg = '*{}#{}* left *{}*.'.format(member.name, member.discriminator, server.name)
 		logLevel = 0
 		await self._logEvent(server, msg, logLevel)
 		
 	async def member_update(self, before, after):
+		if before.bot:
+			return
 		# A member changed something about their user-profile
 		logLevel = 1
 		server = before.server
-		if not str(before.status).lower() == str(after.status).lower():
+		if not str(before.status).lower() == str(after.status).lower() and self.shouldLog('user.status', server):
 			msg = '*{}#{}* went from *{}* to *{}*.'.format(before.name, before.discriminator, str(before.status).lower(), str(after.status).lower())
 			await self._logEvent(server, msg, 2)
 		if not before.game == after.game:
 			# Something changed
 			msg = '*{}#{}* changed playing status: ```\n'.format(before.name, before.discriminator)
-			if not before.game.name == after.game.name:
+			if not before.game.name == after.game.name and self.shouldLog('user.game.name', server):
 				# Name change
 				msg += 'Name:\n   {}\n   --->\n   {}'.format(before.game.name, after.game.name)
-			if not before.game.url == after.game.url:
+			if not before.game.url == after.game.url and self.shouldLog('user.game.url', server):
 				# URL changed
 				msg += 'URL:\n   {}\n   --->\n   {}'.format(before.game.url, after.game.url)
-			if not before.game.type == after.game.type:
+			if not before.game.type == after.game.type and self.shouldLog('user.game.type', server):
 				# Type changed
 				msg += 'Type:\n   {}\n   --->\n   {}'.format(before.game.type, after.game.type)
 			msg += '```'
 			await self._logEvent(server, msg, logLevel)
-		if not before.avatar_url == after.avatar_url:
+		if not before.avatar_url == after.avatar_url and self.shouldLog('user.avatar', server):
 			# Avatar changed
 			msg = '*{}#{}* changed avatars: ```\n{}\n   --->\n{}```'.format(before.name, before.discriminator, before.avatar_url, after.avatar_url)
 			await self._logEvent(server, msg, logLevel)
-		if not before.nick == after.nick:
+		if not before.nick == after.nick and self.shouldLog('user.nick', server):
 			# Nickname changed
 			msg = '*{}#{}* changed nickname: ```\n{}\n   --->\n{}```'.format(before.name, before.discriminator, before.nick, after.nick)
 			await self._logEvent(server, msg, logLevel)
-		if not before.name == after.name:
+		if not before.name == after.name and self.shouldLog('user.name', server):
 			# Name changed
 			msg = '*{}#{}* changed name: ```\n{}\n   --->\n{}```'.format(before.name, before.discriminator, before.name, after.name)
 			await self._logEvent(server, msg, logLevel)
 		
 	async def message(self, message):
+		if message.author.bot:
+			return
+		if not self.shouldLog('message.send', message.server):
+			return
 		# A message was sent
 		msg = '*{}#{}*, in *#{}*, sent: ```\n{}```'.format(message.author.name, message.author.discriminator, message.channel.name, message.content)
 		logLevel = 2
@@ -102,6 +135,10 @@ class Debugging:
 		return { 'Ignore' : False, 'Delete' : False}
 		
 	async def message_edit(self, before, after):
+		if before.author.bot:
+			return
+		if not self.shouldLog('message.edit', before.server):
+			return
 		# A message was edited
 		msg = '*{}#{}*, in *#{}*, edited: ```\n{}```'.format(before.author.name, before.author.discriminator, before.channel.name, before.content)
 		logLevel = 1
@@ -111,6 +148,10 @@ class Debugging:
 		return { 'Ignore' : False, 'Delete' : False}
 		
 	async def message_delete(self, message):
+		if message.author.bot:
+			return
+		if not self.shouldLog('message.delete', message.server):
+			return
 		# A message was deleted
 		msg = '*{}#{}*, in *#{}*, deleted: ```\n{}```'.format(message.author.name, message.author.discriminator, message.channel.name, message.content)
 		logLevel = 1
@@ -126,10 +167,10 @@ class Debugging:
 		if not logChan:
 			return
 		# At this point - we have a log channel - get/compare the log levels
-		logLevel  = self.settings.getServerStat(server, "LogLevel")
+		'''logLevel  = self.settings.getServerStat(server, "LogLevel")
 		if log_level > logLevel:
 			# Too verbose
-			return
+			return'''
 		# At this point - we log the message
 		await self.bot.send_message(logChan, log_message)
 	
@@ -168,6 +209,96 @@ class Debugging:
 				await self.bot.send_message(ctx.message.channel, msg)
 				return
 		await self.bot.send_message(ctx.message.channel, 'Logging is currently *disabled*.')
+		
+		
+	@commands.command(pass_context=True)
+	async def logenable(self, ctx, *, options = None):
+		"""Enables the passed, comma-delimited log vars."""
+		author  = ctx.message.author
+		server  = ctx.message.server
+		channel = ctx.message.channel
+		
+		isAdmin = ctx.message.author.permissions_in(ctx.message.channel).administrator
+		if not isAdmin:
+			checkAdmin = self.settings.getServerStat(ctx.message.server, "AdminArray")
+			for role in ctx.message.author.roles:
+				for aRole in checkAdmin:
+					# Get the role that corresponds to the id
+					if aRole['ID'] == role.id:
+						isAdmin = True
+		# Only allow admins to change server stats
+		if not isAdmin:
+			await self.bot.send_message(ctx.message.channel, 'You do not have sufficient privileges to access this command.')
+			return
+		
+		if options == None:
+			msg = 'Usage: `{}logenable option1, option2, option3...`\nAvailable options:\n{}'.format(ctx.prefix, ', '.join(self.logvars))
+			await self.bot.send_message(ctx.message.channel, msg)
+			return
+		
+		serverOptions = self.settings.getServerStat(server, "LogVars")
+		options.replace(" ", "")
+		optionList = options.split(',')
+		addedOptions = []
+		for option in optionList:
+			if option.lower() in self.logvars and not option.lower() in serverOptions:
+				# Only add if valid and not already added
+				addedOptions.append(option.lower())
+		if not len(addedOptions):
+			await self.bot.send_message(ctx.message.channel, 'No valid or disabled options were passed.')
+			return
+		
+		for option in addedOptions:
+			serverOptions.append(option)
+		
+		if len(addedOptions) == 1:
+			await self.bot.send_message(ctx.message.channel, '*1* logging option enabled.')
+		else:
+			await self.bot.send_message(ctx.message.channel, '*{}* logging options enabled.'.format(len(addedOptions)))
+		
+				
+	@commands.command(pass_context=True)
+	async def logdisable(self, ctx, *, options = None):
+		"""Disables the passed, comma-delimited log vars."""
+		author  = ctx.message.author
+		server  = ctx.message.server
+		channel = ctx.message.channel
+		
+		isAdmin = ctx.message.author.permissions_in(ctx.message.channel).administrator
+		if not isAdmin:
+			checkAdmin = self.settings.getServerStat(ctx.message.server, "AdminArray")
+			for role in ctx.message.author.roles:
+				for aRole in checkAdmin:
+					# Get the role that corresponds to the id
+					if aRole['ID'] == role.id:
+						isAdmin = True
+		# Only allow admins to change server stats
+		if not isAdmin:
+			await self.bot.send_message(ctx.message.channel, 'You do not have sufficient privileges to access this command.')
+			return
+		
+		if options == None:
+			msg = 'Usage: `{}logenable option1, option2, option3...`\nAvailable options:\n{}'.format(ctx.prefix, ', '.join(self.logvars))
+			await self.bot.send_message(ctx.message.channel, msg)
+			return
+		
+		serverOptions = self.settings.getServerStat(server, "LogVars")
+		options.replace(" ", "")
+		optionList = options.split(',')
+		addedOptions = []
+		for option in optionList:
+			if option.lower() in self.logvars and option.lower() in serverOptions:
+				# Only remove if valid and in list
+				addedOptions.append(option.lower())
+				serverOptions.remove(option.lower())
+		if not len(addedOptions):
+			await self.bot.send_message(ctx.message.channel, 'No valid or enabled options were passed.  Nothing to disable.')
+			return
+		
+		if len(addedOptions) == 1:
+			await self.bot.send_message(ctx.message.channel, '*1* logging option disabled.')
+		else:
+			await self.bot.send_message(ctx.message.channel, '*{}* logging options disabled.'.format(len(addedOptions)))			
 		
 	
 	@commands.command(pass_context=True)
