7ac4730c76381805d5bf519c92d1e1fb3922bafb
CorpNewt <corporatenewt@gmail.com>
2017-04-15 16:06:21 -0500

    fixed missing self. tag

--- Cogs/Server.py
+++ Cogs/Server.py
@@ -55,7 +55,7 @@ class Server:
 		"""Output's the server's prefix - custom or otherwise."""
 
 		try:
-			serverPrefix = settings.getServerStat(ctx.message.server, "Prefix")
+			serverPrefix = self.settings.getServerStat(ctx.message.server, "Prefix")
 		except Exception:
 			serverPrefix = None
 
