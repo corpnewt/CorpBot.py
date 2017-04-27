390e596c25dd4900e12998f3dbd91ec3c16087f2
CorpNewt <corporatenewt@gmail.com>
2017-04-27 10:50:36 -0500

    Update PCPP.py

--- Cogs/PCPP.py
+++ Cogs/PCPP.py
@@ -114,7 +114,8 @@ def getMarkdown( url, style = None, escape = False):
 	if not len(types):
 		return None
 	if not len(types) == len(names):
-		return None
+		# Only way this would happen that I've seen so far, is with custom entries
+		return 'custom'
 	partout = ''
 	if style.lower() == 'md':
 		partout = mdStyle(types, names, escape)
