eea6bc9dc3174dbb9fa934dbb512555f5173f25a
CorpNewt <corporatenewt@gmail.com>
2017-04-27 12:53:06 -0500

    Update PCPP.py

--- Cogs/PCPP.py
+++ Cogs/PCPP.py
@@ -97,10 +97,8 @@ def getMarkdown( url, style = None, escape = False):
 	# Experimental crap because developing while not at home
 	table = dom('table.manual-zebra').children('tbody')
 	for child in table.items():
-		for achild in child.items():
-			type = achild.children().eq(0)
-			name = achild.children().eq(2)
-			print("'{}': '{}'".format(type, name))
+		c = child().children().eq(0)
+		print(c)
 		#type = children[0].text_content().strip().replace('\r', '').replace('\n', ' ').replace('\t', ' ')
 		#name = children[2].text_content().strip().replace('\r', '').replace('\n', ' ').replace('\t', ' ')
 		#print('Type: "{}"\nName: "{}"'.format(type, name))
