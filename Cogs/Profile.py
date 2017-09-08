e1a65d10c0ed3a1081a068c2e937391acec61dd0
CorpNewt <corporatenewt@gmail.com>
2017-09-07 22:55:50 -0500

    Add message overflow handling - just in case

--- Cogs/Profile.py
+++ Cogs/Profile.py
@@ -7,6 +7,7 @@ from   Cogs import Settings
 from   Cogs import ReadableTime
 from   Cogs import DisplayName
 from   Cogs import Nullify
+from   Cogs import Message
 
 # This is the profiles module.
 
@@ -354,4 +355,5 @@ class Profile:
 			linkText = Nullify.clean(linkText)
 
 		# Speak the link list while cutting off the end ", "
-		await channel.send(linkText[:-2])
+		# await channel.send(linkText[:-2])
+		await Message.say(self.bot, linkText[:-2], ctx.channel, ctx.author, 1)
