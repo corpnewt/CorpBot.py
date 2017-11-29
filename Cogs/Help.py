c16fbaf380785dc5d237fbf130f1588179cc5c31
CorpNewt <corporatenewt@gmail.com>
2017-11-29 16:17:20 -0600

    Update Help.py

--- Cogs/Help.py
+++ Cogs/Help.py
@@ -207,7 +207,7 @@ class Help:
 				cog_string += "{}{} Cog\n".format("	"*tab_indent_count, cog)
 			for command in cog_commands:
 				cog_string += "{}	{}\n".format("	"*tab_indent_count, prefix + command.signature)
-				cog_string += "{}	└─{}\n".foramt("	"*tab_indent_count, self._get_help(command, 80))
+				cog_string += "{}	└─{}\n".format("	"*tab_indent_count, self._get_help(command, 80))
 			cog_string += "\n"
 			msg += cog_string
 			
