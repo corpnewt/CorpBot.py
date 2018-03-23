5c3d71e0547958b57b71e750d1d39c7a1e69462b
CorpNewt <corporatenewt@gmail.com>
2018-03-22 19:48:51 -0500

    Move convert from google to xe

--- Cogs/Search.py
+++ Cogs/Search.py
@@ -7,6 +7,7 @@ from   discord.ext import commands
 from   Cogs import Settings
 from   Cogs import DisplayName
 from   Cogs import TinyURL
+from   Cogs import Message
 from   Cogs import DL
 from   pyquery import PyQuery as pq
 
@@ -128,15 +129,28 @@ class Search:
 			hasError = True
 		
 		if hasError == True:
-			errorMsg = 'Usage: `$convert <amount> <fromCurrency> <toCurrency>`\n *Here are the list of currencies* ```Markdown\n<AED = United Arab Emirates Dirham (AED)>\n<AFN = Afghan Afghani (AFN)>\n<ALL = Albanian Lek (ALL)>\n<AMD = Armenian Dram (AMD)>\n<ANG = Netherlands Antillean Guilder (ANG)>\n<AOA = Angolan Kwanza (AOA)>\n<ARS = Argentine Peso (ARS)>\n<AUD = Australian Dollar (A$)>\n<AWG = Aruban Florin (AWG)>\n<AZN = Azerbaijani Manat (AZN)>\n<BAM = Bosnia-Herzegovina Convertible Mark (BAM)>\n<BBD = Barbadian Dollar (BBD)>\n<BDT = Bangladeshi Taka (BDT)>\n<BGN = Bulgarian Lev (BGN)>\n<BHD = Bahraini Dinar (BHD)>\n<BIF = Burundian Franc (BIF)>\n<BMD = Bermudan Dollar (BMD)>\n<BND = Brunei Dollar (BND)>\n<BOB = Bolivian Boliviano (BOB)>\n<BRL = Brazilian Real (R$)>\n<BSD = Bahamian Dollar (BSD)>\n<BTC = Bitcoin (฿)>\n<BTN = Bhutanese Ngultrum (BTN)>\n<BWP = Botswanan Pula (BWP)>\n<BYN = Belarusian Ruble (BYN)>\n<BYR = Belarusian Ruble (2000-2016) (BYR)>\n<BZD = Belize Dollar (BZD)>\n<CAD = Canadian Dollar (CA$)>\n<CDF = Congolese Franc (CDF)>\n<CHF = Swiss Franc (CHF)>\n<CLF = Chilean Unit of Account (UF) (CLF)>\n<CLP = Chilean Peso (CLP)>\n```';
-			await ctx.message.author.send(errorMsg)
-			errorMsg = '```Markdown\n<CNH = CNH (CNH)>\n<CNY = Chinese Yuan (CN¥)>\n<COP = Colombian Peso (COP)>\n<CRC = Costa Rican Colón (CRC)>\n<CUP = Cuban Peso (CUP)>\n<CVE = Cape Verdean Escudo (CVE)>\n<CZK = Czech Republic Koruna (CZK)>\n<DEM = German Mark (DEM)>\n<DJF = Djiboutian Franc (DJF)>\n<DKK = Danish Krone (DKK)>\n<DOP = Dominican Peso (DOP)>\n<DZD = Algerian Dinar (DZD)>\n<EGP = Egyptian Pound (EGP)>\n<ERN = Eritrean Nakfa (ERN)>\n<ETB = Ethiopian Birr (ETB)>\n<EUR = Euro (€)>\n<FIM = Finnish Markka (FIM)>\n<FJD = Fijian Dollar (FJD)>\n<FKP = Falkland Islands Pound (FKP)>\n<FRF = French Franc (FRF)>\n<GBP = British Pound (£)>\n<GEL = Georgian Lari (GEL)>\n<GHS = Ghanaian Cedi (GHS)>\n<GIP = Gibraltar Pound (GIP)>\n<GMD = Gambian Dalasi (GMD)>\n<GNF = Guinean Franc (GNF)>\n<GTQ = Guatemalan Quetzal (GTQ)>\n<GYD = Guyanaese Dollar (GYD)>\n<HKD = Hong Kong Dollar (HK$)>\n<HNL = Honduran Lempira (HNL)>\n<HRK = Croatian Kuna (HRK)>\n<HTG = Haitian Gourde (HTG)>\n<HUF = Hungarian Forint (HUF)>\n<IDR = Indonesian Rupiah (IDR)>\n<IEP = Irish Pound (IEP)>\n<ILS = Israeli New Sheqel (₪)>\n<INR = Indian Rupee (₹)>\n<IQD = Iraqi Dinar (IQD)>\n<IRR = Iranian Rial (IRR)>\n<ISK = Icelandic Króna (ISK)>\n<ITL = Italian Lira (ITL)>\n<JMD = Jamaican Dollar (JMD)>\n<JOD = Jordanian Dinar (JOD)>\n<JPY = Japanese Yen (¥)>\n<KES = Kenyan Shilling (KES)>\n<KGS = Kyrgystani Som (KGS)>\n<KHR = Cambodian Riel (KHR)>\n<KMF = Comorian Franc (KMF)>\n```';
-			await ctx.message.author.send(errorMsg)
-			errorMsg = '```Markdown\n<KPW = North Korean Won (KPW)>\n<KRW = South Korean Won (₩)>\n<KWD = Kuwaiti Dinar (KWD)>\n<KYD = Cayman Islands Dollar (KYD)>\n<KZT = Kazakhstani Tenge (KZT)>\n<LAK = Laotian Kip (LAK)>\n<LBP = Lebanese Pound (LBP)>\n<LKR = Sri Lankan Rupee (LKR)>\n<LRD = Liberian Dollar (LRD)>\n<LSL = Lesotho Loti (LSL)>\n<LTL = Lithuanian Litas (LTL)>\n<LVL = Latvian Lats (LVL)>\n<LYD = Libyan Dinar (LYD)>\n<MAD = Moroccan Dirham (MAD)>\n<MDL = Moldovan Leu (MDL)>\n<MGA = Malagasy Ariary (MGA)>\n<MKD = Macedonian Denar (MKD)>\n<MMK = Myanmar Kyat (MMK)>\n<MNT = Mongolian Tugrik (MNT)>\n<MOP = Macanese Pataca (MOP)>\n<MRO = Mauritanian Ouguiya (MRO)>\n<MUR = Mauritian Rupee (MUR)>\n<MVR = Maldivian Rufiyaa (MVR)>\n<MWK = Malawian Kwacha (MWK)>\n<MXN = Mexican Peso (MX$)>\n<MYR = Malaysian Ringgit (MYR)>\n<MZN = Mozambican Metical (MZN)>\n<NAD = Namibian Dollar (NAD)>\n<NGN = Nigerian Naira (NGN)>\n<NIO = Nicaraguan Córdoba (NIO)>\n<NOK = Norwegian Krone (NOK)>\n<NPR = Nepalese Rupee (NPR)>\n<NZD = New Zealand Dollar (NZ$)>\n<OMR = Omani Rial (OMR)>\n<PAB = Panamanian Balboa (PAB)>\n<PEN = Peruvian Nuevo Sol (PEN)>\n<PGK = Papua New Guinean Kina (PGK)>\n```';
-			await ctx.message.author.send(errorMsg)
-			errorMsg = '```Markdown\n<PHP = Philippine Peso (PHP)>\n<PKG = PKG (PKG)>\n<PKR = Pakistani Rupee (PKR)>\n<PLN = Polish Zloty (PLN)>\n<PYG = Paraguayan Guarani (PYG)>\n<QAR = Qatari Rial (QAR)>\n<RON = Romanian Leu (RON)>\n<RSD = Serbian Dinar (RSD)>\n<RUB = Russian Ruble (RUB)>\n<RWF = Rwandan Franc (RWF)>\n<SAR = Saudi Riyal (SAR)>\n<SBD = Solomon Islands Dollar (SBD)>\n<SCR = Seychellois Rupee (SCR)>\n<SDG = Sudanese Pound (SDG)>\n<SEK = Swedish Krona (SEK)>\n<SGD = Singapore Dollar (SGD)>\n<SHP = St. Helena Pound (SHP)>\n<SKK = Slovak Koruna (SKK)>\n<SLL = Sierra Leonean Leone (SLL)>\n<SOS = Somali Shilling (SOS)>\n<SRD = Surinamese Dollar (SRD)>\n<STD = São Tomé & Príncipe Dobra (STD)>\n<SVC = Salvadoran Colón (SVC)>\n<SYP = Syrian Pound (SYP)>\n<SZL = Swazi Lilangeni (SZL)>\n<THB = Thai Baht (THB)>\n<TJS = Tajikistani Somoni (TJS)>\n<TMT = Turkmenistani Manat (TMT)>\n<TND = Tunisian Dinar (TND)>\n<TOP = Tongan Paʻanga (TOP)>\n<TRY = Turkish Lira (TRY)>\n<TTD = Trinidad & Tobago Dollar (TTD)>\n<TWD = New Taiwan Dollar (NT$)>\n<TZS = Tanzanian Shilling (TZS)>\n<UAH = Ukrainian Hryvnia (UAH)>\n<UGX = Ugandan Shilling (UGX)>\n<USD = US Dollar ($)>\n<UYU = Uruguayan Peso (UYU)>\n<UZS = Uzbekistani Som (UZS)>\n<VEF = Venezuelan Bolívar (VEF)>\n<VND = Vietnamese Dong (₫)>\n<VUV = Vanuatu Vatu (VUV)>\n<WST = Samoan Tala (WST)>\n<XAF = Central African CFA Franc (FCFA)>\n<XCD = East Caribbean Dollar (EC$)>\n<XDR = Special Drawing Rights (XDR)>\n<XOF = West African CFA Franc (CFA)>\n<XPF = CFP Franc (CFPF)>\n<YER = Yemeni Rial (YER)>\n<ZAR = South African Rand (ZAR)>\n<ZMK = Zambian Kwacha (1968–2012) (ZMK)>\n<ZMW = Zambian Kwacha (ZMW)>\n<ZWL = Zimbabwean Dollar (2009) (ZWL)>\n```';
-			await ctx.message.author.send(errorMsg)
-			return;
+			r = await DL.async_text("http://www.xe.com/currency/")
+			curr_list = []
+			try:
+				lst = r.split("search-text'>")
+				for l in lst:
+					curr_list.append(l.split("<")[0])
+				if not "XBT - Bitcoin" in curr_list:
+					curr_list.append("XBT - Bitcoin")
+				curr_list = sorted(curr_list)
+			except:
+				curr_list = []
+			await ctx.send("Usage: `{}convert [amount] [from_currency] [to_currency]`".format(ctx.prefix))
+			if len(curr_list):
+				await Message.EmbedText(
+					title="Currency List",
+					description="\n".join(curr_list),
+					desc_head="```\n",
+					desc_foot="```",
+					pm_after=0,
+					color=ctx.author
+				).send(ctx)
+			return
 
 		if"to" in to:
 			to = to.replace("to","")
@@ -145,11 +159,11 @@ class Search:
 		# convert_url = "https://finance.google.com/finance/converter?a={}&from={}&to={}".format(amount,frm,to)
 		convert_url = "http://www.xe.com/currencyconverter/convert/?Amount={}&From={}&To={}".format(amount,frm,to)
 		r = await DL.async_text(convert_url)
+
 		try:
-			r = r.split("uccResultAmount\">")[1]
-			print(len(r))
-			r = r.split("<")[0]
-			results = [r, to]
+			amt = r.split("uccResultAmount'>")[1].split("<")[0]
+			to  = r.split("uccToCurrencyCode'>")[1].split("<")[0]
+			results = [amt, to]
 		except:
 			await ctx.send("Error getting currency conversion results :(")
 			return
@@ -160,7 +174,7 @@ class Search:
 		
 		if len(results):
 			amount = "{:,}".format(int(amount)) if int(amount) == float(amount) else "{:,f}".format(amount).rstrip("0")
-			results[0] = float(results[0])
+			results[0] = float(results[0].replace(",", ""))
 			results[0] = "{:,}".format(int(results[0])) if int(results[0]) == float(results[0]) else "{:,f}".format(results[0]).rstrip("0")
 			await ctx.channel.send("{} {} is {} {}".format(amount,str(frm).upper(),results[0], results[1]))
 		else:
