8acf2c82acbe433379230f2217834c80325e27db
CorpNewt <corporatenewt@gmail.com>
2018-12-22 20:37:42 -0600

    Get vendor name and all device changes

--- Cogs/PciUsb.py
+++ Cogs/PciUsb.py
@@ -17,66 +17,96 @@ class PciUsb:
 	async def pci(self, ctx, ven_dev = None):
 		"""Searches pci-ids.ucw.cz for the passed PCI ven:dev id."""
 		if not ven_dev:
-			await ctx.send("Usage: `{}pci vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}pci vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		try:
 			v,i = ven_dev.split(":")
 		except:
-			await ctx.send("Usage: `{}pci vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}pci vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		if not (len(v)==len(i)==4):
-			await ctx.send("Usage: `{}pci vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}pci vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		if not v.isalnum() and not i.isalnum():
 			await ctx.send("Ven and dev ids must be alphanumeric.")
 			return
+		url = "http://pci-ids.ucw.cz/read/PC/{}".format(v)
+		try:
+			html = await DL.async_text(url)
+		except:
+			await ctx.send("No data returned.")
+			return
+		vendor = None
+		for line in html.split("\n"):
+			if '<div class="name">' in line:
+				try:
+					vendor = line.split("Name: ")[1].split("<")[0].replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
+					break
+				except:
+					pass
+		vendor = v if not vendor else vendor
 		url = "http://pci-ids.ucw.cz/read/PC/{}/{}".format(v,i)
 		try:
 			html = await DL.async_text(url)
 		except:
 			await ctx.send("No data returned.")
 			return
-		out = None
+		out = ""
 		for line in html.split("\n"):
 			if "itemname" in line.lower():
-				out = "Name: ".join(line.split("Name: ")[1:]).replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
-				break
-		if not out:
+				out += "Name: ".join(line.split("Name: ")[1:]).replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
+				out += "\n"
+		if not len(out):
 			await ctx.send("No name found.")
 			return
 		# Got data
-		await Message.EmbedText(description="`{}` = {}".format(ven_dev,out),title="PCI Device Results",footer="Powered by http://pci-ids.ucw.cz",color=ctx.author).send(ctx)
+		await Message.EmbedText(description="`{}`\n\n{}".format(ven_dev,out),title="{} PCI Device Results".format(vendor),footer="Powered by http://pci-ids.ucw.cz",color=ctx.author).send(ctx)
 
 	@commands.command(pass_context=True)
 	async def usb(self, ctx, ven_dev = None):
 		"""Searches usb-ids.gowdy.us for the passed USB ven:dev id."""
 		if not ven_dev:
-			await ctx.send("Usage: `{}usb vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}usb vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		try:
 			v,i = ven_dev.split(":")
 		except:
-			await ctx.send("Usage: `{}usb vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}usb vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		if not (len(v)==len(i)==4):
-			await ctx.send("Usage: `{}usb vvvv:iiii` where `vvvv` is the vendor id, and `iiii` is the device id.".format(ctx.prefix))
+			await ctx.send("Usage: `{}usb vvvv:dddd` where `vvvv` is the vendor id, and `dddd` is the device id.".format(ctx.prefix))
 			return
 		if not v.isalnum() and not i.isalnum():
 			await ctx.send("Ven and dev ids must be alphanumeric.")
 			return
+		url = "https://usb-ids.gowdy.us/read/UD/{}".format(v)
+		try:
+			html = await DL.async_text(url)
+		except:
+			await ctx.send("No data returned.")
+			return
+		vendor = None
+		for line in html.split("\n"):
+			if '<div class="name">' in line:
+				try:
+					vendor = line.split("Name: ")[1].split("<")[0].replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
+					break
+				except:
+					pass
+		vendor = v if not vendor else vendor
 		url = "https://usb-ids.gowdy.us/read/UD/{}/{}".format(v,i)
 		try:
 			html = await DL.async_text(url)
 		except:
 			await ctx.send("No data returned.")
 			return
-		out = None
+		out = ""
 		for line in html.split("\n"):
 			if "itemname" in line.lower():
-				out = "Name: ".join(line.split("Name: ")[1:]).replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
-				break
-		if not out:
+				out += "Name: ".join(line.split("Name: ")[1:]).replace("&amp;","&").replace("&quot;",'"').replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
+				out += "\n"
+		if not len(out):
 			await ctx.send("No name found.")
 			return
 		# Got data
-		await Message.EmbedText(description="`{}` = {}".format(ven_dev,out),title="USB Device Results",footer="Powered by https://usb-ids.gowdy.us",color=ctx.author).send(ctx)
+		await Message.EmbedText(description="`{}`\n\n{}".format(ven_dev,out),title="{} USB Device Results".format(vendor),footer="Powered by https://usb-ids.gowdy.us",color=ctx.author).send(ctx)
