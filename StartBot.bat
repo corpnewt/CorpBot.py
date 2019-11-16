bfb16db49edb71b9df23a3bdff8776abbf439fa8
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2019-11-15 21:08:00 -0600

    Add info for Lavalink

--- StartBot.bat
+++ StartBot.bat
@@ -10,8 +10,28 @@ set "thisDir=%~dp0"
 set "rd=Redis-x64-3.2.100"
 set "redis=redis-server.exe"
 set "start=Start.bat"
+set "ld=Lavalink"
+set "lava=Lavalink.jar"
 set /a wait=5
 
+if EXIST "%thisDir%\%ld%\%lava%" (
+    echo Starting Lavalink server...
+    pushd "%thisDir%\%ld%"
+    start "" java -jar "%lava%"
+    popd
+    echo Waiting %wait% seconds...
+    echo.
+) else (
+    echo "%thisDir%\%ld%\%lava%"
+    echo does not exist!
+    echo.
+    echo You can get it from:
+    echo   https://github.com/Frederikam/Lavalink/releases/latest
+    pause
+    exit /b
+)
+timeout %wait%
+echo.
 if EXIST "%thisDir%\%rd%\%redis%" (
     echo Starting database...
     pushd "%thisDir%\%rd%"
@@ -19,11 +39,17 @@ if EXIST "%thisDir%\%rd%\%redis%" (
     popd
     echo Waiting %wait% seconds...
     echo.
-    timeout %wait%
 ) else (
     echo "%thisDir%\%redis%"
     echo does not exist!
+    echo.
+    echo You can get it from:
+    echo   https://github.com/microsoftarchive/redis/releases/latest
+    echo.
+    pause
+    exit /b
 )
+timeout %wait%
 echo.
 if EXIST "%thisDir%\%start%" (
     echo Starting bot...
@@ -32,6 +58,8 @@ if EXIST "%thisDir%\%start%" (
 ) else (
     echo "%thisDir%\%start%"
     echo does not exist!
+    pause
+    exit /b
 )
 echo.
 echo Done.
