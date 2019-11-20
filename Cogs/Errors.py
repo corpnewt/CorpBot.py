6c9d4f28acf0b8fd836efe335c98230a36a99ef7
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2019-11-20 15:28:16 -0600

    Ignore Music Cog errors

--- Cogs/Errors.py
+++ Cogs/Errors.py
@@ -17,18 +17,19 @@ class Errors(commands.Cog):
 	@commands.Cog.listener()
 	async def on_command_error(self, context, exception):
 		if type(exception) is commands.CommandInvokeError and type(exception.original) is discord.Forbidden:
-			await Message.EmbedText(
+			return await Message.EmbedText(
 					title="⚠ Forbidden Error",
 					color=context.author,
 					description="Looks like I tried to do something I don't have permissions for!\nIf `{}{}` is role-based - make sure that my role is above the affected role(s).".format(context.prefix, context.command.name)
 				).send(context)
+		if str(exception).startswith("Music Cog: "):
+			# Generic music exception - ignore
 			return
 		cog = context.cog
 		if cog:
 			attr = '_{0.__class__.__name__}__error'.format(cog)
 			if hasattr(cog, attr):
 				return
-
 		print('Ignoring exception in command {}:'.format(context.command), file=sys.stderr)
 		traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
 
