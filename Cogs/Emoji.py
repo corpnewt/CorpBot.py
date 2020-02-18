c3b99de3e21450179d0636709c02f28ef476ad63
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-02-18 14:18:14 -0600

    Update Emoji.py

--- Cogs/Emoji.py
+++ Cogs/Emoji.py
@@ -9,6 +9,7 @@ class Emoji(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.max_emojis = 10
 
     def _get_emoji_url(self, emoji):
         if len(emoji) < 3:
@@ -28,7 +29,7 @@ class Emoji(commands.Cog):
 
     @commands.command()
     async def addemoji(self, ctx, *, emoji = None, name = None):
-        '''Adds the passed emoji, url, or attachment as a custom emoji with the passed name (bot-admin only).'''
+        '''Adds the passed emoji, url, or attachment as a custom emoji with the passed name (bot-admin only, max of 10).'''
         if not await Utils.is_bot_admin_reply(ctx): return
         if not len(ctx.message.attachments) and emoji == name == None:
             return await ctx.send("Usage: `{}addemoji [emoji, url, attachment] [name]`".format(ctx.prefix))
@@ -57,8 +58,9 @@ class Emoji(commands.Cog):
             if len(emojis_to_add) and last_name:
                 # Update the previous name if need be
                 emojis_to_add[-1][1] = "".join([z for z in "_".join(last_name) if z.isalnum() or z == "_"])
-            # We have a valid url or emoji here
-            emojis_to_add.append([url[0],url[1]])
+            # We have a valid url or emoji here - let's make sure it's unique
+            if not url[0] in [x[0] for x in emojis_to_add]:
+                emojis_to_add.append([url[0],url[1]])
             # Reset last_name
             last_name = []
         if len(emojis_to_add) and last_name:
@@ -67,8 +69,13 @@ class Emoji(commands.Cog):
         if not emojis_to_add: return await ctx.send("Usage: `{}addemoji [emoji, url, attachment] [name]`".format(ctx.prefix))
         # Now we have a list of emojis and names
         added_emojis = []
-        message = await ctx.send("Adding {} emoji{}...".format(len(emojis_to_add),"" if len(emojis_to_add)==1 else "s"))
-        for emoji_to_add in emojis_to_add:
+        allowed = len(emojis_to_add) if len(emojis_to_add)<=self.max_emojis else self.max_emojis
+        omitted = " ({} omitted, beyond the limit of {})".format(len(emojis_to_add)-self.max_emojis,self.max_emojis) if len(emojis_to_add)>self.max_emojis else ""
+        message = await ctx.send("Adding {} emoji{}{}...".format(
+            allowed,
+            "" if allowed==1 else "s",
+            omitted))
+        for emoji_to_add in emojis_to_add[:self.max_emojis]:
             # Let's try to download it
             emoji,e_name = emoji_to_add # Expand into the parts
             f = await GetImage.download(emoji)
@@ -83,7 +90,11 @@ class Emoji(commands.Cog):
             try: new_emoji = await ctx.guild.create_custom_emoji(name=e_name,image=image,roles=None,reason="Added by {}#{}".format(ctx.author.name,ctx.author.discriminator))
             except: continue
             added_emojis.append(new_emoji)
-        msg = "Created {} of {} emoji{}.".format(len(added_emojis),len(emojis_to_add),"" if len(emojis_to_add)==1 else "s")
+        msg = "Created {} of {} emoji{}{}.".format(
+            len(added_emojis),
+            allowed,"" if allowed==1 else "s",
+            omitted
+            )
         if len(added_emojis):
             msg += "\n\n"
             emoji_text = ["{} - `:{}:`".format(self._get_emoji_mention(x),x.name) for x in added_emojis]
