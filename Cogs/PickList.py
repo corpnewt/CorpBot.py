0b0a38a1b1061aef05cee43ce1e94a04a4670d33
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-03-31 23:06:11 -0500

    Add cancel emoji

--- Cogs/PickList.py
+++ Cogs/PickList.py
@@ -97,7 +97,7 @@ class PagePicker(Picker):
         # Expects self.list to contain the fields needed - each a dict with {"name":name,"value":value,"inline":inline}
         self.max = kwargs.get("max",10) # Must be between 1 and 25
         self.max = 1 if self.max < 1 else 10 if self.max > 10 else self.max
-        self.reactions = ["⏪","◀","▶","⏩","🔢"] # These will always be in the same order
+        self.reactions = ["⏪","◀","▶","⏩","🔢","🛑"] # These will always be in the same order
         self.url = kwargs.get("url",None) # The URL the title of the embed will link to
 
     def _get_page_contents(self, page_number):
@@ -146,6 +146,10 @@ class PagePicker(Picker):
                 return (page, self.self_message)
             # Got a reaction - let's process it
             ind = self.reactions.index(str(reaction.emoji))
+            if ind == 5:
+                # We bailed - let's clear reactions and close it down
+                await self._remove_reactions(self.reactions)
+                return (page, self.self_message)
             page = 0 if ind==0 else page-1 if ind==1 else page+1 if ind==2 else pages if ind==3 else page
             if ind == 4:
                 # User selects a page
