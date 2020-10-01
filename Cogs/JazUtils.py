e4ff6ac7881aac74b11e5bc6cfb758eed5d9c4e1
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-10-01 16:40:40 -0500

    Small fixes + comment out conflicting command

--- Cogs/JazUtils.py
+++ Cogs/JazUtils.py
@@ -32,8 +32,7 @@ class JazUtils(commands.Cog):
 		timestamp = ((sid >> 22) + 1420070400000) / 1000 # python uses seconds not milliseconds
 		cdate = datetime.datetime.utcfromtimestamp(timestamp)
 		msg = "Snowflake created {}".format(cdate.strftime('%A, %B %d %Y at %H:%M:%S UTC'))
-		await ctx.channel.send(msg)
-		return
+		return await ctx.send(msg)
 
 	@commands.command(pass_context=True)
 	async def fullsnowflake(self, ctx, *, sid : str = None):
@@ -57,17 +56,14 @@ class JazUtils(commands.Cog):
 		embed.add_field(name="As custom emote", value="<:test:{}>".format(sid))
 		embed.add_field(name="As animated emote", value="<a:test:{}>".format(sid))
 
-		await ctx.channel.send(embed=embed)
+		await ctx.send(embed=embed)
 
 
 	## role listing and queries
 
 	def role_check(self, user, role_query):
 		# returns True or False if a user has named role
-		for role in user.roles:
-			if role.name in role_query:
-				return True
-		return False
+		return any((role.name in role_query for role in user.roles))
 
 	def alphanum_filter(self, text):
 		# filter for searching a role by name without having to worry about case or punctuation
@@ -75,19 +71,12 @@ class JazUtils(commands.Cog):
 
 	def rolelist_filter(self, roles, id_list):
 		# filters the full role hierarchy based on the predefined lists above
-		out_roles = []
-		for role in roles:
-			if int(role.id) in id_list:
-				out_roles.append(role)
-		return out_roles
+		return [role for role in roles if int(role.id) in id_list]
 
 	def get_named_role(self, server, rolename):
 		# finds a role in a server by name
 		check_name = self.alphanum_filter(rolename)
-		for role in server.roles:
-			if self.alphanum_filter(role.name) == check_name:
-				return role
-		return None
+		return next((role for role in server.roles if self.alphanum_filter(role.name) == check_name),None)
 
 	def role_accumulate(self, check_roles, members):
 		## iterate over the members to accumulate a count of each role
@@ -108,7 +97,7 @@ class JazUtils(commands.Cog):
 		pages = []
 		buildstr = ''
 		for role,count in rlist: # this generates and paginates the info
-			line = '{} {}\n'.format(count,role.mention)
+			line = '{:,} {}\n'.format(count,role.mention)
 			if len(buildstr) + len(line) > EMBED_MAX_LEN:
 				pages.append(buildstr) # split the page here
 				buildstr = line
@@ -118,22 +107,22 @@ class JazUtils(commands.Cog):
 			pages.append(buildstr) #if the string has data not already listed in the pages, add it
 
 		for index,page in enumerate(pages): # enumerate so we can add a page number
-			embed = discord.Embed(title='{} page {}/{}'.format(title, index+1, len(pages)), description=page)
-			await ctx.channel.send(embed=embed)
+			embed = discord.Embed(title='{} page {:,}/{:,}'.format(title, index+1, len(pages)), description=page)
+			await ctx.send(embed=embed)
 
 	@commands.command(pass_context=True)
 	async def roles(self, ctx, sort_order:str='name'):
 		"""Shows roles and their member counts. Takes one argument,
 		sort_order, which can be default, name, count, or rainbow."""
 
+		sort_order = sort_order.lower()
 		if not sort_order in ['default', 'name', 'count', 'rainbow']: # make sure it has valid args
-			await ctx.channel.send("Invalid arguments. Check the help for this command.")
-			return
+			return await ctx.send("Invalid arguments. Check the help for this command.")
 		
-		check_roles = ctx.message.guild.roles # we use roles for these because sometimes we want to see the order
+		check_roles = ctx.guild.roles # we use roles for these because sometimes we want to see the order
 		
 		## now we iterate over the members to accumulate a count of each role
-		rolecounts = self.role_accumulate(check_roles, ctx.message.guild.members)
+		rolecounts = self.role_accumulate(check_roles, ctx.guild.members)
 		
 		sorted_list = []
 		if sort_order == 'default': # default sort = the server role hierarchy
@@ -152,17 +141,16 @@ class JazUtils(commands.Cog):
 
 		await self.rolelist_paginate(ctx,sorted_list) # send the list to get actually printed to discord
 
-	@commands.command(pass_context=True)
+	'''@commands.command(pass_context=True)
 	async def rolecall(self, ctx, *, rolename):
 		"""Counts the number of members with a specific role."""
-		check_role = self.get_named_role(ctx.message.guild, rolename)
+		check_role = self.get_named_role(ctx.guild, rolename)
 		if not check_role:
-			await ctx.channel.send("I can't find that role!")
-			return
+			return await ctx.send("I can't find that role!")
 
 		count = 0
 		online = 0
-		for member in ctx.message.guild.members:
+		for member in ctx.guild.members:
 			if check_role in member.roles:
 				count += 1
 				if member.status != discord.Status.offline:
@@ -170,13 +158,14 @@ class JazUtils(commands.Cog):
 
 		embed = discord.Embed(title=check_role.name, description='{}/{} online'.format(online, count), color=check_role.color)
 		embed.set_footer(text='ID: {}'.format(check_role.id))
-		await ctx.channel.send(embed=embed)
+		await ctx.send(embed=embed)'''
 
 	@commands.command(pass_context=True)
 	async def whohas(self, ctx, *, rolename):
 		"""Lists the people who have the specified role alongside their online status.
 		Can take a -nick or -username argument to enhance output."""
 		mode = 0 # tells how to display: 0 = just mention, 1 = add nickname, 2 = add username
+		rolename = rolename.lower()
 		if '-nick' in rolename:
 			mode = 1
 			rolename = rolename.replace('-nick','')
@@ -184,15 +173,11 @@ class JazUtils(commands.Cog):
 			mode = 2
 			rolename = rolename.replace('-username','')
 
-		check_role = self.get_named_role(ctx.message.guild, rolename)
+		check_role = self.get_named_role(ctx.guild, rolename)
 		if not check_role:
-			await ctx.channel.send("I can't find that role!")
-			return
+			return await ctx.send("I can't find that role!")
 
-		users = []
-		for member in ctx.message.guild.members:
-			if check_role in member.roles:
-				users.append(member)
+		users = [member for member in ctx.guild.members if check_role in member.roles]
 
 		sorted_list = sorted(users, key=lambda usr: (STATUSMAP1.get(usr.status,'4')) + (usr.nick.lower() if usr.nick else usr.name.lower()))
 		truncated = False
@@ -209,21 +194,21 @@ class JazUtils(commands.Cog):
 		if truncated:
 			page += '\n*and {} more...*'.format(len(users) - MAX_USERS)
 
-		embed = discord.Embed(title='{} members with {}'.format(len(users), check_role.name), description=page, color=check_role.color)
+		embed = discord.Embed(title='{:,} members with {}'.format(len(users), check_role.name), description=page, color=check_role.color)
 		embed.set_footer(text='ID: {}'.format(check_role.id))
-		await ctx.channel.send(embed=embed)
+		await ctx.send(embed=embed)
 
 	@commands.command(pass_context=True)
 	async def rolecount(self, ctx):
 		"""Simply counts the number of roles on the server. (excluding @everyone)"""
-		await ctx.channel.send('This server has {} total roles.'.format(len(ctx.message.guild.roles) - 1))
+		await ctx.send('This server has {:,} total roles.'.format(len(ctx.guild.roles) - 1))
 
 	@commands.command(pass_context=True)
 	async def emptyroles(self, ctx):
 		"""Shows a list of roles that have zero members."""
 
-		check_roles = ctx.message.guild.roles # grab in hierarchy order so they're easier to find in the server settings
-		rolecounts = self.role_accumulate(check_roles, ctx.message.guild.members) # same accumulate as the `roles` command
+		check_roles = ctx.guild.roles # grab in hierarchy order so they're easier to find in the server settings
+		rolecounts = self.role_accumulate(check_roles, ctx.guild.members) # same accumulate as the `roles` command
 
 		sorted_list = []
 		for role in check_roles:
@@ -231,6 +216,6 @@ class JazUtils(commands.Cog):
 				sorted_list.append((role, rolecounts.get(role,0)))
 
 		if not sorted_list: # another failsafe
-			await ctx.channel.send('Seems there are no empty roles...')
+			return await ctx.send('Seems there are no empty roles...')
 
 		await self.rolelist_paginate(ctx, sorted_list, title='Empty Roles')
