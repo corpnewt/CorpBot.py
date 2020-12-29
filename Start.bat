4bcc042f8a9626210954b82df89171f21e492f8f
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-12-29 05:16:45 -0600

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,9 +1,10 @@
 @echo off
-setlocal enableDelayedExpansion
+REM Get our local path before delayed expansion - allows ! in path
+set "thisDir=%~dp0"
 
+setlocal enableDelayedExpansion
 REM Setup initial vars
 set "script_name=WatchDog.py"
-set "thisDir=%~dp0"
 set /a tried=0
 set "toask=yes"
 set "py2v="
