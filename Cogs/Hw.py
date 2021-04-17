4c315516c12a1131e7fe4f138f47201a1e343fd6
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2021-04-16 19:27:41 -0500

    Fix accidental empty name

--- Cogs/Hw.py
+++ Cogs/Hw.py
@@ -779,7 +779,7 @@ class Hw(commands.Cog):
 		if not len(buildList):
 			msg = '*{}* has no builds on file!  They can add some with the `{}newhw` command.'.format(DisplayName.name(member), ctx.prefix)
 			return await ctx.send(msg)
-		items = [{"name":x["Name"]+" (Main Build)" if x["Main"] else "","value":Utils.truncate_string(x["Hardware"])} for x in buildList]
+		items = [{"name":x["Name"]+(" (Main Build)" if x["Main"] else ""),"value":Utils.truncate_string(x["Hardware"])} for x in buildList]
 		return await PickList.PagePicker(title="{}'s Builds".format(DisplayName.name(member)),list=items,ctx=ctx).pick()
 
 
