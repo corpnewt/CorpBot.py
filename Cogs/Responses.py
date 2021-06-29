1003f1f12ae7d7a3af38303a527d1868d22ee440
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2021-06-29 07:57:44 -0500

    Migrate to on_message(), expand handlers
    * Move away from the custom `message()` method - and use the built-in `on_message()` event handler to allow pings
    * Add `mvresponse` to allow reordering of the response list (as they're iterated in order and only the first match is evaluated)
    * Add `chkresponse` to allow testing for matches and give a breakdown of response info
    * Add `editresponse` to update regex triggers of existing responses by index

--- Cogs/Responses.py
+++ Cogs/Responses.py
@@ -28,64 +28,87 @@ class Responses(commands.Cog):
 		self.regexBan      = re.compile(r"\[\[ban\]\]",       re.IGNORECASE)
 		self.regexSuppress = re.compile(r"\[\[suppress\]\]",  re.IGNORECASE)
 
-	async def message(self, message):
-		if message.author.bot: return
-		if not message.guild: return
-		message_responses = self.settings.getServerStat(message.guild, "MessageResponses", {})
-		if not message_responses: return
-		# We have something to check
-		ctx = await self.bot.get_context(message)
-		if ctx.command: return # Don't check if we're running a command
+	async def _get_response(self, ctx, message):
+		message_responses = self.settings.getServerStat(ctx.guild, "MessageResponses", {})
+		if not message_responses: return {}
 		# Check for matching response triggers here
-		content = message.content.replace("\n"," ") # Remove newlines for better matching
+		content = message.replace("\n"," ") # Remove newlines for better matching
 		for trigger in message_responses:
 			if not re.fullmatch(trigger, content): continue
+			response = {"matched":trigger}
 			# Got a full match - build the message, send it and bail
 			m = message_responses[trigger]
-			delete = False
-			if not Utils.is_bot_admin(ctx): # Check for non-bot-admin responses: delete, mute, kick, ban
-				delete = True if self.regexDelete.search(m) else False
-				if self.regexBan.search(m): # Start with the most extreme and go from there
-					await message.guild.ban(message.author,reason="Response trigger matched")
-				elif self.regexKick.search(m):
-					await message.guild.kick(message.author,reason="Response trigger matched")
-				elif self.regexMute.search(m):
+			if self.regexDelete.search(m): response["delete"] = True
+			if self.regexSuppress.search(m): response["suppress"] = True
+			action = "ban" if self.regexBan.search(m) else "kick" if self.regexKick.search(m) else "mute" if self.regexMute.search(m) else None
+			if action:
+				response["action"] = action
+				if action == "mute":
 					# Let's get the mute time - if any
-					try: mute_time = int(time.time()) + int(self.regexMute.search(m).group(0).replace("]]","").split(":")[-1])
-					except: mute_time = None
-					mute = self.bot.get_cog("Mute")
-					if mute: await mute._mute(message.author, message.guild, cooldown=mute_time)
-			elif self.regexSuppress.search(m):
-				return # An admin/bot-admin, and we're suppressing output - bail
-			m = re.sub(self.regexUserName, "{}".format(DisplayName.name(message.author)), m)
-			m = re.sub(self.regexUserPing, "{}".format(message.author.mention), m)
+					try: response["mute_time"] = int(self.regexMute.search(m).group(0).replace("]]","").split(":")[-1])
+					except: pass
+			m = re.sub(self.regexUserName, "{}".format(DisplayName.name(ctx.author)), m)
+			m = re.sub(self.regexUserPing, "{}".format(ctx.author.mention), m)
 			m = re.sub(self.regexServer,   "{}".format(Nullify.escape_all(ctx.guild.name)), m)
 			m = re.sub(self.regexHere,     "@here", m)
 			m = re.sub(self.regexEveryone, "@everyone", m)
 			# Strip out leftovers from delete, ban, kick, mute, and suppress
 			for sub in (self.regexDelete,self.regexBan,self.regexKick,self.regexMute,self.regexSuppress):
 				m = re.sub(sub,"",m)
-			return {"Delete":delete,"Respond":m}
+			response["message"] = m
+			return response
+		return {}
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		# Gather exclusions - no bots, no dms, and don't check if running a command
+		if message.author.bot: return
+		if not message.guild: return
+		ctx = await self.bot.get_context(message)
+		if ctx.command: return
+		# Gather the response info - if any
+		response = await self._get_response(ctx,message.content)
+		if not response: return
+		# See if we're admin/bot-admin - and bail if suppressed
+		if Utils.is_bot_admin(ctx) and response.get("suppress"): return
+		# Walk punishments first in order of severity (ban -> kick -> mute)
+		if response.get("action") in ("ban","kick"):
+			action = ctx.guild.ban if response["action"] == "ban" else ctx.guild.kick
+			await action(ctx.author,reason="Response trigger matched")
+		elif response.get("action") == "mute":
+			mute = self.bot.get_cog("Mute")
+			mute_time = None if not response.get("mute_time") else int(time.time())+response["mute_time"]
+			if mute: await mute._mute(ctx.author,ctx.guild,cooldown=mute_time)
+		# Check if we need to delete the message
+		if response.get("delete"):
+			try: await message.delete()
+			except: pass # RIP - couldn't delete that one, I guess
+		if response.get("message","").strip(): # Don't send an empty message, or one with just whitespace
+			return await ctx.send(response["message"],allowed_mentions=discord.AllowedMentions.all())
 
 	@commands.command()
 	async def addresponse(self, ctx, regex_trigger = None, *, response = None):
-		"""Adds a new response for the regex trigger.  If the trigger has spaces, it must be wrapped in quotes (bot-admin only).
-		Available Options:
+		"""Adds a new response for the regex trigger - or updates the response if the trigger exists already.  If the trigger has spaces, it must be wrapped in quotes (bot-admin only).
+		
+		Value substitutions:
 		
 		[[user]]     = user name
 		[[atuser]]   = user mention
 		[[server]]   = server name
 		[[here]]     = @​here ping
 		[[everyone]] = @​everyone ping
-		[[suppress]] = suppresses output for admin/bot-admin author matches
 
-		Non Admin/Bot-Admin Author Options:
+		Standard user behavioral flags (do not apply to admin/bot-admin):
 
 		[[delete]]   = delete the original message
 		[[ban]]      = bans the message author
 		[[kick]]     = kicks the message author
 		[[mute]]     = mutes the author indefinitely
 		[[mute:#]]   = mutes the message author for # seconds
+
+		Admin/bot-admin behavioral flags:
+
+		[[suppress]] = suppresses output for admin/bot-admin author matches
 		
 		Example:  $addresponse "(?i)(hello there|\\btest\\b).*" [[atuser]], this is a test!
 		
@@ -103,6 +126,30 @@ class Responses(commands.Cog):
 		message_responses[regex_trigger] = response
 		self.settings.setServerStat(ctx.guild, "MessageResponses", message_responses)
 		return await ctx.send("{} response trigger!".format(context))
+
+	@commands.command()
+	async def editresponse(self, ctx, response_index = None, *, regex_trigger = None):
+		"""Edits the regex trigger for the passed index (bot-admin only)."""
+
+		if not await Utils.is_bot_admin_reply(ctx): return
+		if not regex_trigger or not response_index: return await ctx.send("Usage: `{}addresponse regex_trigger response`".format(ctx.prefix))
+		message_responses = self.settings.getServerStat(ctx.guild, "MessageResponses", {})
+		if not message_responses: return await ctx.send("No responses setup!  You can use the `{}addresponse` command to add some.".format(ctx.prefix))
+		# Ensure the passed index is valid
+		try:
+			response_index = int(response_index)
+			assert 0 < response_index <= len(message_responses)
+		except:
+			return await ctx.send("You need to pass a valid integer from 1 to {:,}.\nYou can get a numbered list with `{}responses`".format(len(message_responses),ctx.prefix))
+		# Ensure the regex is valid
+		try: re.compile(regex_trigger)
+		except Exception as e: return await ctx.send(Nullify.escape_all(str(e)))
+		# Update the response
+		ordered_responses = {}
+		for index,key in enumerate(message_responses,start=1):
+			ordered_responses[regex_trigger if index==response_index else key] = message_responses[key]
+		self.settings.setServerStat(ctx.guild,"MessageResponses",ordered_responses)
+		return await ctx.send("Updated response trigger at index {:,}!".format(response_index))
 
 	@commands.command()
 	async def responses(self, ctx):
@@ -127,7 +174,7 @@ class Responses(commands.Cog):
 			regex_trigger_number = int(regex_trigger_number)
 			assert 0 < regex_trigger_number <= len(message_responses)
 		except:
-			return await ctx.send("You need to pass a valid number from 1 to {:,}.\nYou can get a numbered list with `{}responses`".format(len(message_responses),ctx.prefix))
+			return await ctx.send("You need to pass a valid integer from 1 to {:,}.\nYou can get a numbered list with `{}responses`".format(len(message_responses),ctx.prefix))
 		# Remove it, save, and report
 		message_responses.pop(list(message_responses)[regex_trigger_number-1],None)
 		self.settings.setServerStat(ctx.guild, "MessageResponses", message_responses)
@@ -140,3 +187,52 @@ class Responses(commands.Cog):
 		if not await Utils.is_bot_admin_reply(ctx): return
 		self.settings.setServerStat(ctx.guild, "MessageResponses", {})
 		return await ctx.send("All response triggers removed!")
+
+	@commands.command()
+	async def mvresponse(self, ctx, response_index = None, target_index = None):
+		"""Moves the passed response index to the target index (bot-admin only)."""
+
+		if not await Utils.is_bot_admin_reply(ctx): return
+		if response_index == None or target_index == None:
+			return await ctx.send("Usage: `{}mvresponse [response_index] [target_index]`\nYou can get a numbered list with `{}responses`".format(ctx.prefix,ctx.prefix))
+		message_responses = self.settings.getServerStat(ctx.guild, "MessageResponses", {})
+		if not message_responses: return await ctx.send("No responses setup!  You can use the `{}addresponse` command to add some.".format(ctx.prefix))
+		# Make sure our indices are within the proper range
+		try:
+			response_index = int(response_index)
+			target_index = int(target_index)
+			assert all((0 < x <= len(message_responses) for x in (response_index,target_index)))
+		except:
+			return await ctx.send("Both `response_index` and `target_index` must be valid intergers from 1 to {:,}.\nYou can get a numbered list with `{}responses`".format(len(message_responses),ctx.prefix))
+		if response_index == target_index: return await ctx.send("Both indices are the same - nothing to move!")
+		# Let's get the keys in a list - remove the target, add it to the desired index, then build a new dict with the elements
+		keys = list(message_responses)
+		keys.insert(target_index-1,keys.pop(response_index-1))
+		ordered_responses = {}
+		for key in keys: ordered_responses[key] = message_responses[key]
+		self.settings.setServerStat(ctx.guild,"MessageResponses",ordered_responses)
+		return await ctx.send("Moved response from {:,} to {:,}!".format(response_index,target_index))
+
+	@commands.command()
+	async def chkresponse(self, ctx, *, check_string = None):
+		"""Reports a breakdown of the first match (if any) in the responses for the passed check string (bot-admin only)."""
+
+		if not await Utils.is_bot_admin_reply(ctx): return
+		if check_string == None: return await ctx.send("Usage: `{}checkresponse [check_string]`\nYou can get a numbered list with `{}responses`".format(ctx.prefix,ctx.prefix))
+		message_responses = self.settings.getServerStat(ctx.guild, "MessageResponses", {})
+		if not message_responses: return await ctx.send("No responses setup!  You can use the `{}addresponse` command to add some.".format(ctx.prefix))
+		response = await self._get_response(ctx,check_string)
+		if not response: return await ctx.send("No matches!")
+		# Got a match - let's print out what it will do
+		description = Nullify.escape_all(response.get("matched","Unknown match"))
+		entries = []
+		# Let's walk the reponse and add values
+		entries.append({"name":"Output Suppressed for Admin/Bot-Admin:","value":"Yes" if response.get("suppress") else "No"})
+		if response.get("action") == "mute":
+			mute_time = "indefinitely" if not response.get("mute_time") else "for {:,} second{}".format(response["mute_time"],"" if response["mute_time"]==1 else "s")
+			entries.append({"name":"Action:","value":"Mute {}".format(mute_time)})
+		else:
+			entries.append({"name":"Action:","value":str(response.get("action")).capitalize()})
+		entries.append({"name":"Delete:","value":"Yes" if response.get("delete") else "No"})
+		entries.append({"name":"Output Message:","value":"None" if not response.get("message","").strip() else response["message"]})
+		return await PickList.PagePicker(title="Matched Response",description=description,list=entries,ctx=ctx).pick()
