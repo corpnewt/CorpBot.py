1e29b3a2eca6c51736a153faa8673c5543c807bb
Yasen <iasenvasilev@yahoo.com>
2021-07-09 21:47:42 +0300

    Add HackAudio cog with layouts/alcid command

---
+++ Cogs/HackAudio.py
@@ -0,0 +1,38 @@
+from   discord.ext import commands
+from   Cogs import DL
+
+def setup(bot):
+    # Add the bot
+    bot.add_cog(HackAudio(bot))
+
+# layouts/alcid is a simple command that allows members to quickly retrieve all the available
+# layouts for a certain audio codec by *reading* the AppleALC GitHub repository
+
+class HackAudio(commands.Cog):
+    
+	# Init with the bot reference
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["alcid"])
+    async def layouts(self, ctx, codec : str = None):
+        """Search the AppleALC repository for available codec layouts"""
+
+        if codec is None:
+            return await ctx.send("Usage: `{}layouts [codec]`".format(ctx.prefix))
+
+        layouts = []
+        req_url = "https://api.github.com/repos/acidanthera/AppleALC/contents/Resources/{}".format(codec.upper())
+        # Try to find available layouts and store them in a list
+        try:
+            response = await DL.async_json(req_url)
+            for file in response:
+                if file["name"].startswith("layout"):
+                    layouts.append(file["name"].replace("layout", "").replace(".xml", ""))
+        # No such directory
+        except:
+            return await ctx.send("I couldn't find that codec!")
+
+        msg = '**Available layouts for codec *{}*:**\nlayout {}'.format(codec.upper(), ', '.join(map(str, sorted(layouts, key=len))))
+        await ctx.send(msg)
+        
