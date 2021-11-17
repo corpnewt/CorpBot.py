f51e0f7905d39939127d1f87f7902b4dc8af7ead
ntwrkobscure <twitchisadeck@gmail.com>
2021-11-17 17:21:31 +0100

    tweak: minor tweaks; replace spaces between xeons/cores with `-`
    - Also, no need to “simplify” the name for the URL pre-fetch, thanks, Dids!

--- Cogs/IntelArk.py
+++ Cogs/IntelArk.py
@@ -5,6 +5,7 @@ from   Cogs import Message
 from   Cogs import DL
 from   Cogs import PickList
 import urllib
+import re
 
 def setup(bot):
 	# Add the bot
@@ -48,8 +49,6 @@ class IntelArk(commands.Cog):
 		if not len(text):
 			await Message.EmbedText(**args).send(ctx)
 			return
-		
-		text = self.simplified_name(text)
 
 		# message = await Message.EmbedText(title="Intel Ark Search",description="Gathering info...",color=ctx.author,footer="Powered by http://ark.intel.com").send(ctx)
 
@@ -201,29 +200,36 @@ class IntelArk(commands.Cog):
 			"(G)": "℠",
 			"CPU": "",
 			"@": "",
-			" ": "%20"
+			" ": "+"
 		}
+
+		capture = re.search(r"((e|i)\d{1}\s\d{3,4}(\w{1}\d{1}?)?)", search_term)
+
+		if capture:
+			capture = capture.group()
+			search_term = search_term.replace(capture, capture.replace(" ", "-"))
 
 		for key, val in replace_dict.items():
 			if key in search_term:
 				search_term = search_term.replace(key, val)
 
-		if not "-" in search_term:
-			sanitised_term = ""
-			sanitised = search_term.split('%20')
+		return search_term
+		# if not "-" in search_term:
+		# 	sanitised_term = ""
+		# 	sanitised = search_term.split('%20')
 
-			if len(sanitised) == 1:
-				return search_term
-			elif len(sanitised) == 2:
-				return "-".join(sanitised)
+		# 	if len(sanitised) == 1:
+		# 		return search_term
+		# 	elif len(sanitised) == 2:
+		# 		return "-".join(sanitised)
 
-			for i in range(len(sanitised)):
-				if i == (len(sanitised) - 2) and not "-" in search_term:
-					sanitised_term += sanitised[i] + '-' + sanitised[i + 1]
-					break
+		# 	for i in range(len(sanitised)):
+		# 		if i == (len(sanitised) - 2) and not "-" in search_term:
+		# 			sanitised_term += sanitised[i] + '-' + sanitised[i + 1]
+		# 			break
 			
-				sanitised_term += sanitised[i] + '%20' if i != (len(sanitised) - 1) else ''
+		# 		sanitised_term += sanitised[i] + '%20' if i != (len(sanitised) - 1) else ''
 
-			return sanitised_term
-		else:
-			return search_term
+		# 	return sanitised_term
+		# else:
+		# 	return search_term
