92f31bc1766fa44ca564f1b8cd0f4214b16c0e2c
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2021-12-07 10:01:43 -0600

    Strip 0 prefix in id

--- Cogs/Humor.py
+++ Cogs/Humor.py
@@ -267,7 +267,7 @@ class Humor(commands.Cog):
 		templates = await self.getTemps()
 		# Check if the template_id is a valid int, and try to load that one
 		try:
-			int(template_id)
+			template_id = str(int(template_id))
 			chosenTemp = next((x for x in templates if x["id"] == template_id),{"id":template_id,"name":template_id,"box_count":len(box_text)})
 			result = await self._get_meme(chosenTemp,box_text)
 		except:
