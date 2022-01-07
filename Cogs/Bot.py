41eefd2e631f05a33b46756b771e8afe74f41ab2
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2022-01-06 18:36:44 -0600

    Beef up channel resolving

--- Cogs/Bot.py
+++ Cogs/Bot.py
@@ -448,8 +448,18 @@ class Bot(commands.Cog):
 		parts = [x for x in message_url.replace("/"," ").split() if len(x)]
 		try: channel_id,message_id = [int(x) for x in parts[-2:]]
 		except: return await ctx.send("Improperly formatted message url!")
+		# Attempt to get the server from parts as well - but default to this one
+		guild = ctx.guild # Default to the current guild if any
+		if len(parts) > 2:
+			if parts[-3].lower() == "@me": # Assume it's a dm
+				guild = self.bot
+			else: # Hope for an guild id integer
+				try: guild = self.bot.get_guild(int(parts[-3]))
+				except: pass
+		if guild is None: # Get the guild if run in one, or fall back to the bot
+			guild = ctx.guild if ctx.guild else self.bot
 		# Let's actually get the channel+message and ensure it exists
-		channel = ctx.guild.get_channel(channel_id)
+		channel = guild.get_channel(channel_id)
 		if not channel: return await ctx.send("I couldn't find the channel connected to that id.")
 		try: message = await channel.fetch_message(message_id)
 		except: return await ctx.send("I couldn't find the message connected to that id.")
