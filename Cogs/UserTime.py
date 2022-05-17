0e73d20d001af61161c642a1795a68d56a099712
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2022-05-16 19:22:12 -0500

    Fix for UTCOffset not working

--- Cogs/UserTime.py
+++ Cogs/UserTime.py
@@ -30,14 +30,17 @@ def getClockForTime(time_string):
 
 def getUserTime(member, settings, time = None, strft = "%Y-%m-%d %I:%M %p", clock = True, force = None):
 	# Returns a dict representing the time from the passed member's perspective
-	offset = force if force else settings.getGlobalUserStat(member,"TimeZone",settings.getGlobalUserStat(member,"UTCOffset",None))
-	if offset is None:
+	if force: offset = force
+	else:
+		offset = settings.getGlobalUserStat(member,"TimeZone",None)
+		if not offset: offset = settings.getGlobalUserStat(member,"UTCOffset",None)
+	if not offset:
 		# No offset or tz - return UTC
 		t = getClockForTime(time.strftime(strft)) if clock else time.strftime(strft)
 		return { "zone" : 'UTC', "time" : t, "vanity" : "{} {}".format(t,"UTC") }
 	# At this point - we need to determine if we have an offset - or possibly a timezone passed
 	t = getTimeFromTZ(offset, time, strft, clock)
-	if t is None:
+	if not t:
 		# We did not get a zone
 		t = getTimeFromOffset(offset, time, strft, clock)
 	t["vanity"] = "{} {}".format(t["time"],t["zone"])
@@ -56,7 +59,7 @@ def getTimeFromOffset(offset, t = None, strft = "%Y-%m-%d %I:%M %p", clock = Tru
 			return None
 	msg = 'UTC'
 	# Get the time
-	if t is None:
+	if not t:
 		t = datetime.datetime.utcnow()
 	# Apply offset
 	if hours > 0:
@@ -83,7 +86,7 @@ def getTimeFromTZ(tz, t = None, strft = "%Y-%m-%d %I:%M %p", clock = True):
 	# Assume sanitized zones - as they're pulled from pytz
 	# Let's get the timezone list
 	zone = next((pytz.timezone(x) for x in pytz.all_timezones if x.lower() == tz.lower()),None)
-	if zone is None: return
-	zone_now = datetime.datetime.now(zone) if t is None else t.replace(tzinfo=pytz.utc).astimezone(zone)
+	if not zone: return
+	zone_now = t.replace(tzinfo=pytz.utc).astimezone(zone) if t else datetime.datetime.now(zone)
 	ti = getClockForTime(zone_now.strftime(strft)) if clock else zone_now.strftime(strft)
 	return { "zone" : str(zone), "time" : ti}
