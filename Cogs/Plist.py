75f7ab577d7bb755db5310c15aa16c4d39cf0d15
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2022-08-08 06:00:48 -0500

    Fix outdated code

--- Cogs/Plist.py
+++ Cogs/Plist.py
@@ -1,5 +1,5 @@
 from   discord.ext import commands
-import os, re, tempfile, shutil, plistlib
+import re, plistlib
 from Cogs import DL, Message, PickList
 
 def setup(bot):
@@ -14,38 +14,6 @@ class Plist(commands.Cog):
         self.bot = bot
         self.settings = settings
         self.nv_link = "https://gfe.nvidia.com/mac-update"
-
-    async def download(self, url):
-        url = url.strip("<>")
-        # Set up a temp directory
-        dirpath = tempfile.mkdtemp()
-        tempFileName = url.rsplit('/', 1)[-1]
-        # Strip question mark
-        tempFileName = tempFileName.split('?')[0]
-        imagePath = dirpath + "/" + tempFileName
-        rImage = None
-        
-        try:
-            rImage = await DL.async_dl(url)
-        except:
-            pass
-        if not rImage:
-            self.remove(dirpath)
-            return None
-
-        with open(imagePath, 'wb') as f:
-            f.write(rImage)
-
-        # Check if the file exists
-        if not os.path.exists(imagePath):
-            self.remove(dirpath)
-            return None
-
-        return imagePath
-        
-    def remove(self, path):
-        if not path == None and os.path.exists(path):
-            shutil.rmtree(os.path.dirname(path), ignore_errors=True)
             
     def get_os(self, build_number):
         # Returns the best-guess OS version for the build number
@@ -81,15 +49,14 @@ class Plist(commands.Cog):
             data = await DL.async_dl(self.nv_link)
             plist_data = plistlib.loads(data)
         except:
-            await Message.EmbedText(
+            return await Message.Embed(
                 title="⚠ An error occurred!", 
                 description="I guess I couldn't get the manifest...\n\"{}\" may no longer be valid.".format(self.nv_link),
                 color=ctx.author
             ).send(ctx)
-            return
         # We have the plist data
         wd = None
-        if os_build == None:
+        if os_build is None:
             # We just need the newest - let's give everything a numeric value
             alpha = "abcdefghijklmnopqrstuvwxyz"
             new_items = []
@@ -97,12 +64,11 @@ class Plist(commands.Cog):
                 new_items.append({"update" : x, "value" : self.get_value(x["OS"])})
             sorted_list = sorted(new_items, key=lambda x:x["value"], reverse=True)
             if not len(sorted_list):
-                await Message.EmbedText(
+                return await Message.Embed(
                     title="⚠ An error occurred!", 
                     description="There were no updates found at \"{}\".".format(self.nv_link),
                     color=ctx.author
                 ).send(ctx)
-                return
             wd = [{
                     "name": "{} ({})".format(
                         self.get_os(sorted_list[0]["update"]["OS"]),
@@ -140,12 +106,11 @@ class Plist(commands.Cog):
                                 "inline": False
                             })
             if not wd:
-                await Message.EmbedText(
+                return await Message.Embed(
                     title="⚠ An error occurred!", 
                     description="There were no web drivers found for \"{}\".".format(os_build),
                     color=ctx.author
                 ).send(ctx)
-                return
         await PickList.PagePicker(
             title="NVIDIA Web Driver Results For \"{}\" ({} total)".format(os_build if os_build != None else "Latest", len(wd)),
             list=wd,
@@ -159,25 +124,20 @@ class Plist(commands.Cog):
     @commands.command(pass_context=True)
     async def plist(self, ctx, *, url = None):
         """Validates plist file structure.  Accepts a url - or picks the first attachment."""
-        if url == None and len(ctx.message.attachments) == 0:
-            await ctx.send("Usage: `{}plist [url or attachment]`".format(ctx.prefix))
-            return
-
-        if url == None:
-            url = ctx.message.attachments[0].url
-            
-        message = await Message.Embed(description="Downloading...", color=ctx.author).send(ctx)
-        
-        path = await self.download(url)
-        if not path:
-            await Message.Embed(title="⚠ An error occurred!", description="I guess I couldn't get that plist...  Make sure you're passing a valid url or attachment.").edit(ctx, message)
-            return
-        
+        if url is None and not len(ctx.message.attachments):
+            return await ctx.send("Usage: `{}plist [url or attachment]`".format(ctx.prefix))
+        url = url or ctx.message.attachments[0].url
+        message = await Message.Embed(description="Downloading...",color=ctx.author).send(ctx)
         try:
-            plist_data = plistlib.readPlist(path)
+            data = await DL.async_dl(url)
+        except:
+            return await Message.Embed(
+                title="⚠ An error occurred!", 
+                description="I guess I couldn't get that plist...  Make sure you're passing a valid url or attachment.",
+                color=ctx.author
+            ).send(ctx,message)
+        try:
+            plist_data = plistlib.loads(data)
         except Exception as e:
-            await Message.Embed(title="❌ Plist format invalid!", description=str(e)).edit(ctx, message)
-            self.remove(path)
-            return
-        await Message.Embed(title="✅ Plist format OK!").edit(ctx, message)
-        self.remove(path)
+            return await Message.Embed(title="❌ Plist format invalid!",description=str(e),color=ctx.author).send(ctx, message)
+        await Message.Embed(title="✅ Plist format OK!",color=ctx.author).send(ctx, message)
