4429a81b141881c1c9db24507f54fb431dfad9bf
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2023-01-25 16:36:50 -0600

    Add aliases

--- Cogs/Hw.py
+++ Cogs/Hw.py
@@ -26,7 +26,7 @@ class Hw(commands.Cog):
 		# manages to do this twice in < 1 second)
 		return str(time.time())
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwcancel"])
 	async def cancelhw(self, ctx):
 		"""Cancels a current hardware session."""
 		if str(ctx.author.id) in self.hwactive:
@@ -39,7 +39,7 @@ class Hw(commands.Cog):
 		if str(author.id) in self.hwactive:
 			del self.hwactive[str(author.id)]
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def sethwchannel(self, ctx, *, channel: discord.TextChannel = None):
 		"""Sets the channel for hardware (admin only)."""
 		
@@ -62,7 +62,7 @@ class Hw(commands.Cog):
 		msg = 'sethwchannel Error: {}'.format(error)
 		await ctx.send(msg)
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def pcpp(self, ctx, url = None, style = None, escape = None):
 		"""Convert a pcpartpicker.com link into markdown parts. Available styles: normal, md, mdblock, bold, and bolditalic."""
 		usage = "Usage: `{}pcpp [url] [style=normal, md, mdblock, bold, bolditalic] [escape=yes/no (optional)]`".format(ctx.prefix)
@@ -84,7 +84,7 @@ class Hw(commands.Cog):
 			return await ctx.send(msg)
 		await ctx.send(Utils.suppressed(ctx,output))
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwmain"])
 	async def mainhw(self, ctx, *, build = None):
 		"""Sets a new main build from your build list."""
 
@@ -138,7 +138,7 @@ class Hw(commands.Cog):
 		await ctx.send(msg)
 
 	
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwdel","remhw","hwrem"])
 	async def delhw(self, ctx, *, build = None):
 		"""Removes a build from your build list."""
 
@@ -176,7 +176,7 @@ class Hw(commands.Cog):
 		await ctx.send(msg)
 
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwedit"])
 	async def edithw(self, ctx, *, build = None):
 		"""Edits a build from your build list."""
 		hwChannel = None
@@ -322,7 +322,7 @@ class Hw(commands.Cog):
 		await hwChannel.send(msg)
 
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwren","renamehw","hwrename"])
 	async def renhw(self, ctx, *, build = None):
 		"""Renames a build from your build list."""
 		hwChannel = None
@@ -431,7 +431,7 @@ class Hw(commands.Cog):
 		await hwChannel.send(msg)
 		
 		
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwget","searchhw","hwsearch"])
 	async def gethw(self, ctx, *, user = None, search = None):
 		"""Searches the user's hardware for a specific search term."""
 		if not user:
@@ -485,7 +485,7 @@ class Hw(commands.Cog):
 		return await Message.EmbedText(title="Nothing found for that search.",color=ctx.author).send(ctx)
 
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def hw(self, ctx, *, user : str = None, build = None):
 		"""Lists the hardware for either the user's default build - or the passed build."""
 		if not user:
@@ -610,7 +610,7 @@ class Hw(commands.Cog):
 		await ctx.send(Utils.suppressed(ctx,msg))
 
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwraw"])
 	async def rawhw(self, ctx, *, user : str = None, build = None):
 		"""Lists the raw markdown for either the user's default build - or the passed build."""
 		if not user:
@@ -733,7 +733,7 @@ class Hw(commands.Cog):
 		await ctx.send(Utils.suppressed(ctx,msg))
 			
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwlist"])
 	async def listhw(self, ctx, *, user = None):
 		"""Lists the builds for the specified user - or yourself if no user passed."""
 		usage = 'Usage: `{}listhw [user]`'.format(ctx.prefix)
@@ -752,7 +752,7 @@ class Hw(commands.Cog):
 		items = [{"name":"{}. {}".format(i,x["Name"]+(" (Main Build)" if x["Main"] else "")),"value":Utils.truncate_string(x["Hardware"])} for i,x in enumerate(buildList,start=1)]
 		return await PickList.PagePicker(title="{}'s Builds ({:,} total)".format(DisplayName.name(member),len(buildList)),list=items,ctx=ctx).pick()
 
-	@commands.command()
+	@commands.command(aliases=["hwl"])
 	async def lhw(self, ctx, *, user = None):
 		"""Lists only the titles of the builds for the specified user - or yourself if no user passed."""
 		usage = 'Usage: `{}lhw [user]`'.format(ctx.prefix)
@@ -771,7 +771,7 @@ class Hw(commands.Cog):
 			ctx=ctx
 		).pick()
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["hwnew"])
 	async def newhw(self, ctx):
 		"""Initiate a new-hardware conversation with the bot.  The hardware added will also be set as the Main Build."""
 		buildList = self.settings.getGlobalUserStat(ctx.author, "Hardware")
