358a78557c47c35033a46a75e8dcf1428034de19
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2023-08-15 22:15:24 -0500

    Add $conv, $currency, $curr aliases to $convert

--- Cogs/Search.py
+++ Cogs/Search.py
@@ -211,7 +211,7 @@ class Search(commands.Cog):
 			ctx=ctx
 		).pick()
 
-	@commands.command()
+	@commands.command(aliases=["conv","currency","curr"])
 	async def convert(self, ctx, *, amount = None, frm = None, to = None):
 		"""Convert currencies.  If run with no values, the script will print a list of available currencies."""
 		
