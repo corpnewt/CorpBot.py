db1edb595c3fc112bda435f612f231f34fadde5a
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2023-12-26 07:49:19 -0600

    Install the latest commit of py-cord[voice] and pomice

--- Install.py
+++ Install.py
@@ -152,12 +152,14 @@ class Run:
 if __name__ == '__main__':
     r = Run()
     modules = [
-        {"name":"pomice"},
+        # Get the latest commit of pomice to ensure we have Lavalink v4 changes
+        {"name":"pomice","item":"git+https://github.com/cloudwithax/pomice.git"},
         # Remove the updated discord.py that pomice overwrites, and
         # remove py-cord so we can reinstall it with the proper version
         {"name":"discord.py","uninstall":True},
         {"name":"pycord","item":"py-cord[voice]","uninstall":True},
-        {"name":"pycord","item":"py-cord[voice]"},
+        # Get the latest commit of py-cord to ensure we have pyhton 3.12 changes
+        {"name":"pycord","item":"git+https://github.com/Pycord-Development/pycord.git#egg=py-cord[voice]"},
         {"name":"certifi"},
         {"name":"pillow"},
         {"name":"Requests"},
@@ -173,7 +175,6 @@ if __name__ == '__main__':
         {"name":"giphypop","item":"git+https://github.com/shaunduncan/giphypop.git#egg=giphypop"},
         {"name":"numpy"},
         {"name":"pymongo"},
-        {"name":"redis (locked to 3.5.3 for the time being until v4.x is sorted)","item":"redis==3.5.3"},
         {"name":"geopy"},
         {"name":"pyfiglet"},
         {"name":"regex"}
