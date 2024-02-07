81f7a3ef1c379c4f667cfcf3497ed0572e750e52
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-02-07 15:57:46 -0600

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -19,7 +19,7 @@ REM use_py3:
 REM   TRUE  = Use if found, use py2 otherwise
 REM   FALSE = Use py2
 REM   FORCE = Use py3
-set "use_py3=TRUE"
+set "use_py3=FORCE"
 
 REM We'll parse if the first argument passed is
 REM --install-python and if so, we'll just install
