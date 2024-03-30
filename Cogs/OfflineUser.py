542779ab4a9cd1a0c5ed3070a55536005a9e823f
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-03-30 15:36:06 -0500

    Add global per-user RemindOffline with $afk command

--- Cogs/OfflineUser.py
+++ Cogs/OfflineUser.py
@@ -20,12 +20,14 @@ class OfflineUser(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
+		if message.author.bot:
+			return
 		if not message.guild:
 			return
 		# Valid message
 		if not len(message.mentions):
 			return
-		name_list = [x.mention for x in message.mentions if x.status is discord.Status.offline]
+		name_list = [x for x in message.mentions if x != message.author and x.status is discord.Status.offline]
 		if not len(name_list):
 			# No one was offline
 			return
@@ -35,14 +37,42 @@ class OfflineUser(commands.Cog):
 			return
 		# We got a message in a server, and at least one user was mentioned
 		if not self.settings.getServerStat(message.guild, "RemindOffline"):
-			return
+			# We're not mentioning server-wide, let's see if any of the offline users
+			# have the setting set
+			personal_list = [x for x in name_list if self.settings.getGlobalUserStat(x,"RemindOffline",False)]
+			if not personal_list:
+				return
+			# Update the name_list
+			name_list = personal_list
 		if len(name_list) == 1:
-			msg = "It looks like {} is offline - dm them if urgent.".format(name_list[0])
+			msg = "It looks like {} is offline - pm them if urgent.".format(name_list[0].mention)
 		else:
-			msg = "It looks like the following users are offline - dm them if urgent:\n\n{}".format("\n".join(name_list))
-		await ctx.send(msg,reference=message)
+			msg = "It looks like the following users are offline - pm them if urgent:\n\n{}".format("\n".join([x.mention for x in name_list]))
+		await ctx.send(msg,reference=message,mention_author=True)
 
-	@commands.command(pass_context=True)
+	@commands.command()
+	async def afk(self, ctx, *, yes_no = None):
+		"""Sets whether the bot should reply to pings stating you're offline regardless of the per-server $remindoffline settings."""
+		display_name = "Personal offline reminder"
+		current = self.settings.getGlobalUserStat(ctx.author,"RemindOffline",False)
+		if yes_no is None:
+			# Output what we have
+			return await ctx.send("{} currently *{}*.".format(display_name,"enabled" if current else "disabled"))
+		elif yes_no.lower() in ("1","yes","on","true","enabled","enable"):
+			yes_no = True
+			msg = "{} {} *enabled*.".format(display_name,"remains" if current else "is now")
+		elif yes_no.lower() in ("0","no","off","false","disabled","disable"):
+			yes_no = False
+			msg = "{} {} *disabled*.".format(display_name,"is now" if current else "remains")
+		else:
+			msg = "That's not a valid setting."
+			yes_no = current
+		if yes_no != current:
+			# We've updated it
+			self.settings.setGlobalUserStat(ctx.author,"RemindOffline",yes_no)
+		return await ctx.send(msg)
+
+	@commands.command()
 	async def remindoffline(self, ctx, *, yes_no = None):
 		"""Sets whether to inform users that pinged members are offline or not."""
 		if not await Utils.is_bot_admin_reply(ctx): return
