a965e5b6b0de809b1bdeeb38729fc868eea8114d
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-05-31 21:14:50 -0500

    Migrate to plusminus for $calc

--- Cogs/Calc.py
+++ Cogs/Calc.py
@@ -1,155 +1,61 @@
-from __future__ import division
-import asyncio
-import discord
-import random
-from   discord.ext import commands
-from   Cogs import Settings, Utils
-
-from pyparsing import (Literal,CaselessLiteral,Word,Combine,Group,Optional,
-                    ZeroOrMore,Forward,nums,alphas,oneOf)
-import math
-import operator
+from plusminus import ArithmeticParser
+import regex as re
+from discord.ext import commands
 
 def setup(bot):
     # Add the bot
     bot.add_cog(Calc(bot))
 
-__author__='Paul McGuire'
-__version__ = '$Revision: 0.0 $'
-__date__ = '$Date: 2009-03-20 $'
-__source__='''http://pyparsing.wikispaces.com/file/view/fourFn.py
-http://pyparsing.wikispaces.com/message/view/home/15549426
-'''
-__note__='''
-All I've done is rewrap Paul McGuire's fourFn.py as a class, so I can use it
-more easily in other places.
-'''
-
-class NumericStringParser(object):
-    '''
-    Most of this code comes from the fourFn.py pyparsing example
-
-    '''
-    def pushFirst(self, strg, loc, toks ):
-        self.exprStack.append( toks[0] )
-    def pushUMinus(self, strg, loc, toks ):
-        if toks and toks[0]=='-': 
-            self.exprStack.append( 'unary -' )
-    def __init__(self):
-        """
-        expop   :: '^'
-        multop  :: 'x' | '*' | '/'
-        addop   :: '+' | '-'
-        integer :: ['+' | '-'] '0'..'9'+
-        atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
-        factor  :: atom [ expop factor ]*
-        term    :: factor [ multop factor ]*
-        expr    :: term [ addop term ]*
-        """
-        point = Literal( "." )
-        e     = CaselessLiteral( "E" )
-        fnumber = Combine( Word( "+-"+nums, nums ) + 
-                        Optional( point + Optional( Word( nums ) ) ) +
-                        Optional( e + Word( "+-"+nums, nums ) ) )
-        ident = Word(alphas, alphas+nums+"_$")       
-        plus  = Literal( "+" )
-        minus = Literal( "-" )
-        mult  = Literal( "x" )
-        div   = Literal( "/" )
-        lpar  = Literal( "(" ).suppress()
-        rpar  = Literal( ")" ).suppress()
-        addop  = plus | minus
-        multop = mult | div
-        expop = Literal( "^" )
-        pi    = CaselessLiteral( "PI" )
-        expr = Forward()
-        atom = ((Optional(oneOf("- +")) +
-                (pi|e|fnumber|ident+lpar+expr+rpar).setParseAction(self.pushFirst))
-                | Optional(oneOf("- +")) + Group(lpar+expr+rpar)
-                ).setParseAction(self.pushUMinus)       
-        # by defining exponentiation as "atom [ ^ factor ]..." instead of 
-        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
-        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-        factor = Forward()
-        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( self.pushFirst ) )
-        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( self.pushFirst ) )
-        expr << term + ZeroOrMore( ( addop + term ).setParseAction( self.pushFirst ) )
-        # addop_term = ( addop + term ).setParseAction( self.pushFirst )
-        # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
-        # expr <<  general_term       
-        self.bnf = expr
-        # map operator symbols to corresponding arithmetic operations
-        epsilon = 1e-12
-        self.opn = { "+" : operator.add,
-                "-" : operator.sub,
-                "x" : operator.mul,
-                "/" : operator.truediv,
-                "^" : operator.pow }
-        self.fn  = { "sin" : math.sin,
-                "cos" : math.cos,
-                "tan" : math.tan,
-                "abs" : abs,
-                "trunc" : lambda a: int(a),
-                "round" : round,
-                "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
-    def evaluateStack(self, s ):
-        op = s.pop()
-        if op == 'unary -':
-            return -self.evaluateStack( s )
-        if op in "+-x/^":
-            op2 = self.evaluateStack( s )
-            op1 = self.evaluateStack( s )
-            return self.opn[op]( op1, op2 )
-        elif op == "PI":
-            return math.pi # 3.1415926535
-        elif op == "E":
-            return math.e  # 2.718281828
-        elif op in self.fn:
-            return self.fn[op]( self.evaluateStack( s ) )
-        elif op[0].isalpha():
-            return 0
-        else:
-            return float( op )
-    def eval(self,num_string,parseAll=True):
-        self.exprStack=[]
-        results=self.bnf.parseString(num_string,parseAll)
-        val=self.evaluateStack( self.exprStack[:] )
-        return val
+class CustomArithmeticParser(ArithmeticParser):
+    
+    def customize(self):
+        self.add_operator("&",2,ArithmeticParser.LEFT,lambda a,b:a&b)
+        self.add_operator("|",2,ArithmeticParser.LEFT,lambda a,b:a|b)
+        self.add_operator("^",2,ArithmeticParser.LEFT,lambda a,b:a^b)
+        self.add_operator("~",1,ArithmeticParser.RIGHT,lambda a:~a)
+        self.add_operator("<<",2,ArithmeticParser.LEFT,lambda a,b:a<<b)
+        self.add_operator(">>",2,ArithmeticParser.LEFT,lambda a,b:a>>b)
 
 class Calc(commands.Cog):
 
-    # Init with the bot reference, and a reference to the settings var
     def __init__(self, bot):
         self.bot = bot
-        self.nsp=NumericStringParser()
-        global Utils
-        Utils = self.bot.get_cog("Utils")
+        self.hex_re = re.compile(r"(?i)(0x|#)[0-9a-f]+")
 
     @commands.command(aliases=["calculate","math"])
     async def calc(self, ctx, *, formula = None):
         """Do some math."""
 
-        if formula == None: return await ctx.send('Usage: `{}calc [formula]`'.format(ctx.prefix))
-        formula = formula.replace("*","x")
-
+        if formula is None:
+            return await ctx.send('Usage: `{}calc [formula]`'.format(ctx.prefix))
+        parser_lines = [x.strip() for x in formula.replace(";","\n").split("\n") if x.strip()]
+        if not parser_lines:
+            return await ctx.send('Usage: `{}calc [formula]`'.format(ctx.prefix))
+        clean_lines = "\n".join(x.replace("`","").replace("\\","") for x in parser_lines)
+        parser = CustomArithmeticParser()
         try:
-            answer=self.nsp.eval(formula)
-        except:
-            msg = 'I couldn\'t parse "{}" :(\n\n'.format(formula.replace('*', '\\*').replace('`', '\\`').replace('_', '\\_'))
-            msg += 'I understand the following syntax:\n```\n'
-            msg += "expop   :: '^'\n"
-            msg += "multop  :: 'x' | '*' | '/'\n"
-            msg += "addop   :: '+' | '-'\n"
-            msg += "integer :: ['+' | '-'] '0'..'9'+\n"
-            msg += "atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'\n"
-            msg += "factor  :: atom [ expop factor ]*\n"
-            msg += "term    :: factor [ multop factor ]*\n"
-            msg += "expr    :: term [ addop term ]*```"
-            return await ctx.send(Utils.suppressed(ctx,msg))
-        
-        if int(answer) == answer:
-            # Check if it's a whole number and cast to int if so
-            answer = int(answer)
-            
-        # Say message
-        await ctx.send('{} = {}'.format(formula, answer))
+            for line in parser_lines:
+                # Let's regex our way through 0x and # integers
+                for m in re.finditer(self.hex_re,line):
+                    try:
+                        hex_int = int(m.group(0).replace("#","0x"),16)
+                        line = line.replace(m.group(0),str(hex_int))
+                    except:
+                        pass
+                result = parser.evaluate(line)
+        except Exception as e:
+            msg  = 'I couldn\'t parse that formula :(\n'
+            msg += "```\n{}\n```\n".format(str(e).replace("`",""))
+            msg += 'Please see [this page](<https://github.com/pyparsing/plusminus/blob/master/doc/arithmetic_parser.md>) for parsing info.\n\n'
+            msg += '__Additional syntax supported:__\n'
+            msg += '* Newlines or `;` characters separate lines passed to the parser\n'
+            msg += '* `0x` or `#` prefixes denote hexadecimal values\n'
+            msg += '* `&` for bitwise AND\n'
+            msg += '* `|` for bitwise OR\n'
+            msg += '* `^` for bitwise XOR\n'
+            msg += '* `~` for bitwise NOT\n'
+            msg += '* `<<` bit shift left\n'
+            msg += '* `>>` bit shift right'
+            return await ctx.send(msg)
+        # Send the results
+        await ctx.send("```\n{}\n```\n=\n```\n{}\n```".format(clean_lines,result))
