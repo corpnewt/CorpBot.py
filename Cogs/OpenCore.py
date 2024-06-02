cd01ee60e3365419426800e5e665f2087f277160
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-06-02 16:36:46 -0500

    Move $plist and $nvweb to OpenCore Cog, list plist info when found

--- Cogs/OpenCore.py
+++ Cogs/OpenCore.py
@@ -40,6 +40,7 @@ class OpenCore(commands.Cog):
 			"VT2020_2021":["VT2020","VT2021"]
 		}
 		self.regex = re.compile(r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?")
+		self.nv_link = "https://gfe.nvidia.com/mac-update"
 		global Utils
 		Utils = self.bot.get_cog("Utils")
 
@@ -216,6 +217,220 @@ class OpenCore(commands.Cog):
 			m["revisions"] = ", ".join(self.alc_codecs[matched].get("Revisions",[]))
 			m["alternate"] = ", ".join(self.alc_alternate.get(matched,[]))
 		return m
+			
+	def get_os(self, build_number):
+		# Returns the best-guess OS version for the build number
+		alpha = "abcdefghijklmnopqrstuvwxyz"
+		os_version = "Unknown"
+		major = minor = ""
+		try:
+			# Formula looks like this:  AAB; AA - 4 = 10.## version
+			# B index in "ABCDEFGHIJKLMNOPQRSTUVXYZ" = 10.##.## version
+			split = re.findall(r"[^\W\d_]+|\d+", build_number)
+			major = int(split[0])-4
+			minor = alpha.index(split[1].lower())
+			os_version = "10.{}.{}".format(major, minor)
+		except:
+			pass
+		return os_version
+	
+	def get_value(self, build_number):
+		alpha = "abcdefghijklmnopqrstuvwxyz"
+		# Split them up
+		split = re.findall(r"[^\W\d_]+|\d+", build_number)
+		start = split[0].rjust(4, "0")
+		alph  = split[1]
+		end   = split[2].rjust(6, "0")
+		alpha_num = str(alpha.index(alph.lower())).rjust(2, "0")
+		return int(start + alpha_num + end)
+			
+	@commands.command(pass_context=True)
+	async def nvweb(self, ctx, os_build = None):
+		"""Prints the download url for the passed OS build number (if it exists).  If no build number is passed, prints the newest web driver link."""
+		# Get the current manifest
+		link = self.nv_link
+		try:
+			data = await DL.async_dl(self.nv_link)
+			plist_data = plistlib.loads(data)
+			# Ensure the data is loaded, and contains the 
+			# updates key with at least one child, then save
+			# a local copy
+			assert len(plist_data["updates"])
+			with open("nvweb.plist","wb") as f:
+				plistlib.dump(plist_data, f)
+		except:
+			# Load a local file if we have one
+			try:
+				with open("nvweb.plist","rb") as f:
+					plist_data = plistlib.load(f)
+				link = "local nvweb.plist"
+			except:
+				return await Message.Embed(
+					title="⚠ An error occurred!", 
+					description="I guess I couldn't get the manifest...\n{} may no longer be valid.".format(self.nv_link),
+					color=ctx.author
+				).send(ctx)
+		# We have the plist data
+		wd = None
+		if os_build is None:
+			# We just need the newest - let's give everything a numeric value
+			alpha = "abcdefghijklmnopqrstuvwxyz"
+			new_items = []
+			for x in plist_data.get("updates", []):
+				new_items.append({"update" : x, "value" : self.get_value(x["OS"])})
+			sorted_list = sorted(new_items, key=lambda x:x["value"], reverse=True)
+			if not len(sorted_list):
+				return await Message.Embed(
+					title="⚠ An error occurred!", 
+					description="There were no updates found {} {}.".format(
+						"at" if link == self.nv_link else "in",
+						link
+					),
+					color=ctx.author
+				).send(ctx)
+			wd = [{
+					"name": "{} ({})".format(
+						self.get_os(sorted_list[0]["update"]["OS"]),
+						sorted_list[0]["update"]["OS"]),
+					"value": "└─ [{}]({})".format(
+						sorted_list[0]["update"]["version"],
+						sorted_list[0]["update"]["downloadURL"]
+					),
+					"inline": False
+				}]
+		else:
+			# We need to find it
+			mwd = next((x for x in plist_data.get("updates", []) if x["OS"].lower() == os_build.lower()), None)
+			if mwd:
+				wd = [{
+					"name": "{} ({})".format(self.get_os(mwd["OS"]), mwd["OS"]),
+					"value": "└─ [{}]({})".format(mwd["version"], mwd["downloadURL"]),
+					"inline": False
+				}]
+			else:
+				# We didn't get an exact match, let's try to determine what's up
+				# First check if it's a build number (##N####) or OS number (10.##.##)
+				p = os_build.split(".")
+				p = [x for x in p if x != ""]
+				if len(p) > 1 and len(p) < 4:
+					# We have . separated stuffs
+					wd_list = [x for x in plist_data.get("updates", []) if self.get_os(x["OS"]).startswith(os_build)]
+					if len(wd_list):
+						# We got some matches
+						wd = []
+						for x,i in enumerate(wd_list,start=1):
+							wd.append({
+								"name": "{}. {} ({})".format(x,self.get_os(i["OS"]), i["OS"]),
+								"value": "└─ [{}]({})".format(i["version"], i["downloadURL"]),
+								"inline": False
+							})
+			if not wd:
+				return await Message.Embed(
+					title="⚠ An error occurred!", 
+					description="There were no web drivers found for \"{}\".".format(os_build),
+					color=ctx.author
+				).send(ctx)
+		await PickList.PagePicker(
+			title="NVIDIA Web Driver Results For \"{}\" ({} total)".format(os_build if os_build != None else "Latest", len(wd)),
+			list=wd,
+			color=ctx.author,
+			pm_after_fields=25,
+			footer="All links pulled from {}".format(link),
+			ctx=ctx
+		).pick()
+
+	@commands.command(pass_context=True)
+	async def plist(self, ctx, *, url = None):
+		"""Validates plist file structure.  Accepts a url - or picks the first attachment."""
+		if url is None and not len(ctx.message.attachments):
+			return await ctx.send("Usage: `{}plist [url or attachment]`".format(ctx.prefix))
+		url = url or ctx.message.attachments[0].url
+		message = await Message.Embed(description="Downloading...",color=ctx.author).send(ctx)
+		try:
+			data = await DL.async_dl(url)
+		except:
+			return await Message.Embed(
+				title="⚠ An error occurred!", 
+				description="I guess I couldn't get that plist...  Make sure you're passing a valid url or attachment.",
+				color=ctx.author
+			).send(ctx,message)
+		try:
+			plist_data = plistlib.loads(data)
+		except Exception as e:
+			return await Message.Embed(title="❌ Plist format invalid!",description=str(e),color=ctx.author).send(ctx, message)
+		
+		# Check for OpenCore specifics - list out entries enabled in
+		# ACPI -> Add, ACPI -> Patch, Kernel -> Add, Kernel -> Patch,
+		# Misc -> Tools, UEFI -> Drivers
+		title = "✅ Plist format OK!"
+		cont  = "'".join(str(type(plist_data)).split("<class '")[1].split("'")[:-1])
+		desc  = "Plist contents are of type: {}".format(cont)
+		if not isinstance(plist_data,dict):
+			# Not a dict - nothing really to check
+			return await Message.Embed(
+				title=title,
+				description=desc,
+				color=ctx.author
+			).send(ctx, message)
+		else:
+			# Now we walk our entries to list them as needed
+			acpi_add     = [x for x in plist_data.get("ACPI",{}).get("Add",[]) if isinstance(x,dict) and x.get("Enabled")]
+			acpi_patch   = [x for x in plist_data.get("ACPI",{}).get("Patch",[]) if isinstance(x,dict) and x.get("Enabled")]
+			kernel_add   = [x for x in plist_data.get("Kernel",{}).get("Add",[]) if isinstance(x,dict) and x.get("Enabled")]
+			kernel_patch = [x for x in plist_data.get("Kernel",{}).get("Patch",[]) if isinstance(x,dict) and x.get("Enabled")]
+			misc_tools   = [x for x in plist_data.get("Misc",{}).get("Tools",[]) if isinstance(x,dict) and x.get("Enabled")]
+			uefi_drivers = [x for x in plist_data.get("UEFI",{}).get("Drivers",[]) if (isinstance(x,dict) and x.get("Enabled")) or isinstance(x,str)]
+
+			if any((acpi_add,acpi_patch,kernel_add,kernel_patch,misc_tools,uefi_drivers)):
+				desc += "\nPlist appears to belong to OpenCore"
+				names_data = (
+					("ACPI -> Add",acpi_add,"Path"),
+					("ACPI -> Patch",acpi_patch,"Comment"),
+					("Kernel -> Add",kernel_add,"BundlePath"),
+					("Kernel -> Patch",kernel_patch,"Comment"),
+					("Misc -> Tools",misc_tools,"Path"),
+					("UEFI -> Drivers",uefi_drivers,"Path")
+				)
+				driver_warning = " - [Using Older OC Schema]" if all(isinstance(x,str) for x in uefi_drivers) \
+							else " - [Using mixed OC Schema!]" if any(isinstance(x,str) for x in uefi_drivers) else ""
+				for n,d,k in names_data:
+					desc += "\n### {} ({:,}){}{}\n".format(
+						n,
+						len(d),
+						driver_warning if n=="UEFI -> Drivers" else "",
+						":" if len(d) else ""
+					)
+					for i,v in enumerate(d,start=1):
+						desc += "\n{}. `{}`".format(
+							i,
+							v.get(k,"'{}' key not present!").format(k) if isinstance(v,dict) else v
+						)
+						# Check for min/max kernel and add them as needed
+						try:
+							min_kernel = v.get("MinKernel","")
+							max_kernel = v.get("MaxKernel","")
+							match_kernel = v.get("MatchKernel","")
+						except:
+							continue
+						if not any((min_kernel,max_kernel,match_kernel)):
+							continue
+						# We got *something* - check min/max first
+						if min_kernel and max_kernel:
+							desc += " ({} -> {})".format(min_kernel,max_kernel)
+						elif min_kernel:
+							desc += " ({}+)".format(min_kernel)
+						elif max_kernel:
+							desc += " ({}-)".format(max_kernel)
+						elif match_kernel:
+							desc += " (MatchKernel {})".format(match_kernel)
+		await PickList.PagePicker(
+			title=title,
+			description=desc,
+			max=20,
+			color=ctx.author,
+			message=message,
+			ctx=ctx
+		).pick()
 
 	@commands.command(aliases=["updatecodecs"])
 	async def getcodecs(self, ctx):
