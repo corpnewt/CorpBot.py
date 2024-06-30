9d8ba432001b7b941e27cc5cf24c4eb7a8e8adbe
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-06-30 12:12:25 -0500

    Reformat and rework

--- Cogs/Reddit.py
+++ Cogs/Reddit.py
@@ -35,12 +35,9 @@ class MLStripper(HTMLParser):
 class Reddit(commands.Cog):
 
 	# Init with the bot reference, and a reference to the settings var and xp var
-	def __init__(self, bot, settings, posts : int = 100):
+	def __init__(self, bot, settings):
 		self.bot = bot
 		self.settings = settings
-		if not type(posts) == int:
-			posts = 100
-		self.posts = posts
 		self.ua = 'CorpNewt DeepThoughtBot'
 		self.extList = ["jpg", "jpeg", "png", "gif", "tiff", "tif"]
 		self.headList = ["image/jpeg", "image/png", "image/gif", "image/jpg"]
@@ -62,20 +59,32 @@ class Reddit(commands.Cog):
 		s.feed(html)
 		return s.get_data()
 	
+	
+	async def getImageHEAD(self, url):
+		response = await DL.async_head_json(url)
+		return response['content-type']
+
+
 	async def getImageHEAD(self, url):
 		response = await DL.async_head_json(url)
 		return response['content-type']
 
 	async def getTitle(self, url, answer : bool = False, image : bool = False):
-		# Load url - with self.posts number of posts
-		r = await DL.async_json(url, {'User-agent': self.ua})
+		try:
+			r = await DL.async_json(url, {'User-agent': self.ua})
+			numPosts = len(r['data']['children'])
+		except Exception:
+			numPosts = 0
+		if numPosts <= 0:
+			# No links
+			return None
 		# If we need an image - make sure we have a valid one
 		gotLink = False
 		while not gotLink:
 			if image:
 				gotImage = False
 				while not gotImage:
-					randnum = random.randint(0,self.posts)
+					randnum = random.randint(0,numPosts-1)
 					try:
 						theJSON = r["data"]["children"][randnum]["data"]
 					except IndexError:
@@ -85,7 +94,7 @@ class Reddit(commands.Cog):
 						gotImage = True
 						gotLink  = True
 			else:
-				randnum = random.randint(0,self.posts)
+				randnum = random.randint(0,numPosts-1)
 				try:
 					theJSON = r["data"]["children"][randnum]["data"]
 					gotLink = True
@@ -93,17 +102,23 @@ class Reddit(commands.Cog):
 					theJSON = { "url" : "" }
 		if not (answer or image):
 			# Just return the title
-			return '{}'.format(self.unescape(theJSON["title"]))
+			return '{} ([Go To Thread](<{}>))'.format(self.unescape(theJSON["title"]),theJSON["url"])
 		if answer or image:
 			# We need the image or the answer
 			return {'title' : self.unescape(theJSON['title']), 'url' : theJSON["url"]}
 
 	async def getText(self, url):
-		# Load url - with self.posts number of posts
-		r = await DL.async_json(url, {'User-agent': self.ua})
+		try:
+			r = await DL.async_json(url, {'User-agent': self.ua})
+			numPosts = len(r['data']['children'])
+		except Exception:
+			numPosts = 0
+		if numPosts <= 0:
+			# No links
+			return None
 		returnDict = None
 		for i in range(0, 10):
-			randnum = random.randint(0,self.posts)
+			randnum = random.randint(0,numPosts-1)
 			try:
 				theJSON = r["data"]["children"][randnum]["data"]
 				returnDict = {
@@ -153,6 +168,11 @@ class Reddit(commands.Cog):
 						# Not an imgur album - let's try for a single image
 						if GetImage.get_ext(imageURL).lower() in self.extList:
 							theURL = imageURL
+							theURL = imageURL
+						elif await self.getImageHEAD(imageURL).lower() in self.headList:
+							# Check header as a last resort
+							theURL = imageURL				
+							theURL = imageURL		
 						elif await self.getImageHEAD(imageURL).lower() in self.headList:
 							# Check header as a last resort
 							theURL = imageURL				
@@ -181,7 +201,7 @@ class Reddit(commands.Cog):
 		self.settings.setServerStat(server, "LastPicture", int(time.time()))
 		return True
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def ruser(self, ctx, *, user_name = None):
 		"""Gets some info on the passed username - attempts to use your username if none provided."""
 		user_name = user_name or ctx.author.display_name
@@ -218,11 +238,16 @@ class Reddit(commands.Cog):
 		# Send the embed
 		await Message.Embed(**e).send(ctx)
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def nosleep(self, ctx):
 		"""I hope you're not tired..."""
+		message = await Message.Embed(
+			title="Fumbling through reddit posts...",
+			color=ctx.author
+		).send(ctx)
 		msg = await self.getText('https://www.reddit.com/r/nosleep/top.json?sort=top&t=week&limit=100')
-		if not msg: return await ctx.send("Whoops! I couldn't find a working link.")
+		if not msg:
+			return await Message.Embed(title="Whoops! I couldn't find a working link.").send(ctx.message)
 		return await PickList.PagePicker(
 			url=msg["url"],
 			title=msg["title"],
@@ -232,14 +257,20 @@ class Reddit(commands.Cog):
 			footer="Score: {:,} | Comments: {:,}".format(
 				msg["score"],
 				msg["num_comments"]
-			)
+			),
+			message=message
 		).pick()
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def joke(self, ctx):
 		"""Let's see if reddit can be funny..."""
+		message = await Message.Embed(
+			title="Fumbling through reddit posts...",
+			color=ctx.author
+		).send(ctx)
 		msg = await self.getText('https://www.reddit.com/r/jokes/top.json?sort=top&t=week&limit=100')
-		if not msg: return await ctx.send("Whoops! I couldn't find a working link.")
+		if not msg:
+			return await Message.Embed(title="Whoops! I couldn't find a working link.").send(ctx,message)
 		# Check for nsfw - and for now, only allow admins/botadmins to post those
 		if msg['over_18']:
 			# NSFW - check admin
@@ -253,98 +284,91 @@ class Reddit(commands.Cog):
 			footer="Score: {:,} | Comments: {:,}".format(
 				msg["score"],
 				msg["num_comments"]
-			)
-		).pick()
-
-	@commands.command(pass_context=True)
-	async def dirtyjoke(self, ctx):
-		"""Let's see if reddit can be dir-... oh... uh.. funny... (bot-admin only)"""
-		# NSFW - check admin
-		if not await Utils.is_bot_admin_reply(ctx,message="You do not have sufficient privileges to access nsfw subreddits."): return
-		msg = await self.getText('https://www.reddit.com/r/DirtyJokes/top.json?sort=top&t=week&limit=100')
-		if not msg: return await ctx.send("Whoops! I couldn't find a working link.")
-		return await PickList.PagePicker(
-			url=msg["url"],
-			title=msg["title"],
-			description=msg["content"],
-			timeout=600, # Allow 10 minutes before we stop watching the picker
-			ctx=ctx,
-			footer="Score: {:,} | Comments: {:,}".format(
-				msg["score"],
-				msg["num_comments"]
-			)
+			),
+			message=message
 		).pick()
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def lpt(self, ctx):
 		"""Become a pro - AT LIFE."""
 		msg = await self.getTitle('https://www.reddit.com/r/LifeProTips/top.json?sort=top&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def shittylpt(self, ctx):
 		"""Your advise is bad, and you should feel bad."""
 		msg = await self.getTitle('https://www.reddit.com/r/ShittyLifeProTips/top.json?sort=top&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def thinkdeep(self, ctx):
 		"""Spout out some intellectual brilliance."""
 		msg = await self.getTitle('https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def brainfart(self, ctx):
 		"""Spout out some uh... intellectual brilliance..."""
 		msg = await self.getTitle('https://www.reddit.com/r/Showerthoughts/controversial.json?sort=controversial&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def nocontext(self, ctx):
 		"""Spout out some intersexual brilliance."""
 		msg = await self.getTitle('https://www.reddit.com/r/nocontext/top.json?sort=top&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def withcontext(self, ctx):
 		"""Spout out some contextual brilliance."""
 		msg = await self.getTitle('https://www.reddit.com/r/evenwithcontext/top.json?sort=top&t=week&limit=100')
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def question(self, ctx):
 		"""Spout out some interstellar questioning... ?"""
 		infoDict = await self.getTitle('https://www.reddit.com/r/NoStupidQuestions/top.json?sort=top&t=week&limit=100', True)
 		self.settings.setServerStat(ctx.guild, "LastAnswer", infoDict["url"])
-		msg = '{}'.format(infoDict["title"])
+		msg = '{} ([Go To Thread](<{}>))'.format(infoDict["title"],infoDict["url"])
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def answer(self, ctx):
 		"""Spout out some interstellar answering... ?"""
 		answer = self.settings.getServerStat(ctx.guild, "LastAnswer")
 		msg = "You need to ask a `{}question` first!".format(ctx.prefix) if not answer else "{}".format(answer)
 		await ctx.send(Nullify.escape_all(msg,markdown=False))
 
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["rimage","reddit"])
 	async def redditimage(self, ctx, subreddit = None):
 		"""Try to grab an image from an image-based subreddit."""
-		if not self.canDisplay(ctx.guild): return
 		if not subreddit: return await ctx.send("You need to pass a subreddit name.")
+		await self._image_do(ctx,'https://www.reddit.com/r/{}'.format(quote(subreddit)))
+		
+	async def _image_do(self, ctx, url, get_top=True):
+		if not self.canDisplay(ctx.guild): return
 		# Grab our image title and url
-		infoDict = await self.getInfo('https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=week&limit=100')
-		if not infoDict: return await ctx.send("Whoops! I couldn't find a working link.")
+		message = await Message.Embed(
+			title="Fumbling through reddit posts...",
+			color=ctx.author
+		).send(ctx)
+		infoDict = None
+		if get_top:
+			for t in ("week","month","year",""):
+				u = "{}/top.json?sort=top&{}limit=100".format(
+					url,
+					"t={}&".format(t) if t else ""
+				)
+				infoDict = await self.getInfo(u)
+				if infoDict: break
+		else:
+			infoDict = await self.getInfo(url)
+		if not infoDict:
+			return await Message.Embed(title="Whoops! I couldn't find a working link.").send(ctx,message)
 		# Check for nsfw - and for now, only allow admins/botadmins to post those
 		if infoDict['over_18']:
 			# NSFW - check admin
 			if not await Utils.is_bot_admin_reply(ctx,message="You do not have sufficient privileges to access nsfw subreddits."): return
-		await Message.Embed(title=infoDict["title"], url="https://www.reddit.com"+infoDict["permalink"], image=infoDict["url"], color=ctx.author).send(ctx)
-
-	async def _image_do(self, ctx, url):
-		if not self.canDisplay(ctx.guild): return
-		# Grab our image title and url
-		infoDict = await self.getInfo(url)
-		if not infoDict: return await ctx.send("Whoops! I couldn't find a working link.")
 		return await Message.Embed(
 			title=infoDict["title"], 
 			url="https://www.reddit.com"+infoDict["permalink"], 
@@ -353,99 +377,99 @@ class Reddit(commands.Cog):
 			footer="Score: {:,} | Comments: {:,}".format(
 				infoDict["score"],
 				infoDict["num_comments"]
-			)).send(ctx)
+			)).send(ctx,message)
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def beeple(self, ctx):
 		"""A new image every day... for years."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/beeple/new.json?limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/beeple/new.json?limit=100', get_top=False)
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def macsetup(self, ctx):
 		"""Feast your eyes upon these setups."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/macsetups/top.json?sort=top&t=week&limit=100')	
+		await self._image_do(ctx,'https://www.reddit.com/r/macsetups')
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def pun(self, ctx):
 		"""I don't know, don't ask..."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/puns/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/puns')
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def carmod(self, ctx):
 		"""Marvels of modern engineering."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/Shitty_Car_Mods/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/Shitty_Car_Mods')
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def battlestation(self, ctx):
 		"""Let's look at some pretty stuff."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/battlestations/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/battlestations')
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def shittybattlestation(self, ctx):
 		"""Let's look at some shitty stuff."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/shittybattlestations/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/shittybattlestations')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def dankmeme(self, ctx):
 		"""Only the dankest."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/dankmemes/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/dankmemes')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def cablefail(self, ctx):
 		"""Might as well be a noose..."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/cablefail/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/cablefail')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def techsupport(self, ctx):
 		"""Tech support irl."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/techsupportgore/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/techsupportgore')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def software(self, ctx):
 		"""I uh... I wrote it myself."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/softwaregore/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/softwaregore')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def meirl(self, ctx):
 		"""Me in real life."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/me_irl/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/me_irl')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def starterpack(self, ctx):
 		"""Starterpacks."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/starterpacks/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/starterpacks')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def earthporn(self, ctx):
 		"""Earth is good."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/EarthPorn/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/EarthPorn')
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def wallpaper(self, ctx):
 		"""Get something pretty to look at."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/wallpapers/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/wallpapers')
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def abandoned(self, ctx):
 		"""Get something abandoned to look at."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/abandonedporn/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/abandonedporn')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def dragon(self, ctx):
 		"""From the past - when great winged beasts soared the skies."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/BeardedDragons/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/BeardedDragons')
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def aww(self, ctx):
 		"""Whenever you're down - uppify."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/aww/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/aww')
 	
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def randomdog(self, ctx):
 		"""Bark if you know whassup."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/dogpictures/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/dogpictures')
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def randomcat(self, ctx):
 		"""Meow."""
-		await self._image_do(ctx, 'https://www.reddit.com/r/cats/top.json?sort=top&t=week&limit=100')
+		await self._image_do(ctx,'https://www.reddit.com/r/cats')
