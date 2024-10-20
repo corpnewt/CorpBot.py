c4ac30144906f4743dd38d0be181109636e5b040
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-10-20 10:40:36 -0500

    Move to page picker, add aliases, code cleanup

--- Cogs/Profile.py
+++ Cogs/Profile.py
@@ -1,7 +1,7 @@
 import asyncio, discord, time
 from   operator import itemgetter
 from   discord.ext import commands
-from   Cogs import Utils, Settings, ReadableTime, DisplayName, Message, Nullify
+from   Cogs import Utils, Settings, ReadableTime, DisplayName, Message, Nullify, PickList
 
 def setup(bot):
 	# Add the bot and deps
@@ -21,12 +21,12 @@ class Profile(commands.Cog):
 		DisplayName = self.bot.get_cog("DisplayName")
 
 		
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["newprofile"])
 	async def addprofile(self, ctx, name = None, *, link = None):
 		"""Add a profile to your profile list."""
 		# Remove tabs, newlines, and carriage returns and strip leading/trailing spaces from the name
-		name = None if name == None else name.replace("\n"," ").replace("\r","").replace("\t"," ").strip()
-		if name == None or link == None:
+		name = None if name is None else name.replace("\n"," ").replace("\r","").replace("\t"," ").strip()
+		if name is None or link is None:
 			msg = 'Usage: `{}addprofile "[profile name]" [link]`'.format(ctx.prefix)
 			return await ctx.send(msg)
 		itemList = self.settings.getUserStat(ctx.author, ctx.guild, "Profiles")
@@ -45,11 +45,11 @@ class Profile(commands.Cog):
 		await ctx.send(msg)
 		
 		
-	@commands.command(pass_context=True)
+	@commands.command(aliases=["remprofile","delprofile","deleteprofile"])
 	async def removeprofile(self, ctx, *, name = None):
 		"""Remove a profile from your profile list."""
-		name = None if name == None else name.replace("\n"," ").replace("\r","").replace("\t"," ").strip()
-		if name == None:
+		name = None if name is None else name.replace("\n"," ").replace("\r","").replace("\t"," ").strip()
+		if name is None:
 			msg = 'Usage: `{}removeprofile [profile name]`'.format(ctx.prefix)
 			return await ctx.send(msg)
 
@@ -90,10 +90,15 @@ class Profile(commands.Cog):
 			msg = "Usage: `{}{}profile [member] [profile name]`".format(ctx.prefix, "raw" if raw else "")
 			return await ctx.send(msg)
 		item = self._get_profile(ctx,name)
-		if item == None:
+		if item is None:
 			return await ctx.send("Sorry, I couldn't find that user/profile.")
 		member,item = item
-		msg = '*{}\'s {} Profile:*\n\n{}'.format(DisplayName.name(member), item['Name'], discord.utils.escape_markdown(item['URL']) if raw else item['URL'])
+		msg = '*{}\'s {}{} Profile:*\n\n{}'.format(
+			DisplayName.name(member),
+			"Raw " if raw else "",
+			Nullify.escape_all(item['Name']),
+			discord.utils.escape_markdown(item['URL']) if raw else item['URL']
+		)
 		return await ctx.send(Utils.suppressed(ctx,msg))
 
 	async def _list_profiles(self,ctx,member=None,raw=None):
@@ -112,28 +117,43 @@ class Profile(commands.Cog):
 			msg = '*{}* has no profiles set!  They can add some with the `{}addprofile "[profile name]" [link]` command!'.format(DisplayName.name(ctx.author), ctx.prefix)
 			return await ctx.send(msg)
 		itemList = sorted(itemList, key=itemgetter('Name'))
-		itemText = "*{}'s* Profiles:\n\n".format(DisplayName.name(member))
-		itemText += discord.utils.escape_markdown("\n".join([x["Name"] for x in itemList])) if raw else "\n".join([x["Name"] for x in itemList])
-		return await Message.Message(message=Utils.suppressed(ctx,itemText)).send(ctx)
+		title="{}'s {}Profiles ({:,} total)".format(
+			DisplayName.name(member),
+			"Raw " if raw else "",
+			len(itemList)
+		)
+		items = []
+		for i,x in enumerate(itemList,start=1):
+			items.append(
+				"{}. {}".format(
+					i,
+					discord.utils.escape_markdown(x["Name"]) if raw else x["Name"]
+				)
+			)
+		return await PickList.PagePicker(
+			title=title,
+			description="\n".join(items),
+			ctx=ctx
+		).pick()
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def profile(self, ctx, *, member = None, name = None):
 		"""Retrieve a profile from the passed user's profile list."""
 		await self._get_profile_reply(ctx,member)
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def rawprofile(self, ctx, *, member = None, name = None):
 		"""Retrieve a profile's raw markdown from the passed user's profile list."""
 		await self._get_profile_reply(ctx,member,raw=True)
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def profileinfo(self, ctx, *, member = None, name = None):
 		"""Displays info about a profile from the passed user's profile list."""
 		if not member:
 			msg = 'Usage: `{}profileinfo [member] [profile name]`'.format(ctx.prefix)
 			return await ctx.send(msg)
 		item = self._get_profile(ctx,member)
-		if item == None:
+		if item is None:
 			return await ctx.send("Sorry, I couldn't find that user/profile.")
 		member,item = item
 		# We have a profile
@@ -144,12 +164,12 @@ class Profile(commands.Cog):
 			msg += "Updated: {} ago\n".format(ReadableTime.getReadableTimeBetween(item["Updated"], current_time, True))
 		return await ctx.send(Utils.suppressed(ctx,msg))
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def profiles(self, ctx, *, member = None):
 		"""List all profiles in the passed user's profile list."""
 		await self._list_profiles(ctx,member)
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def rawprofiles(self, ctx, *, member = None):
 		"""List all profiles' raw markdown in the passed user's profile list."""
 		await self._list_profiles(ctx,member,raw=True)
