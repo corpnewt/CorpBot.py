b29daa58a2c1e6450b8cad4738f2202626aac52a
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2025-10-06 05:08:36 -0500

    Add additional newline character code

--- Cogs/Reddit.py
+++ Cogs/Reddit.py
@@ -740,7 +740,7 @@ class Reddit(commands.Cog):
 							# Ensure we prefix appropriately
 							quoting = True
 							l = "> "+l
-					if l.lower() in ("-","&nbsp;","&#x200b;","#"):
+					if l.lower() in ("-","&nbsp;","&#x200b;","#","&#8203;"):
 						l = "" # Just a newline placeholder
 					if self.re_line.match(l):
 						l = "⎯"*10 # Originally a line separator
